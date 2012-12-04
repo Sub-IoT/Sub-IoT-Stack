c10637d65d10d1bc0a42692c00adb7c9a69c04ad
Glenn Ergeerts <glenn.ergeerts@artesis.be>
2012-12-04 09:41:28 +0100

    moved parsing to separate thread

---
+++ tools/logger/logparser.h
@@ -0,0 +1,31 @@
+#ifndef LOGPARSER_H
+#define LOGPARSER_H
+
+#include <QObject>
+#include <QQueue>
+
+#include <serialport/serialport.h>
+
+QT_USE_NAMESPACE_SERIALPORT
+
+class LogParser : public QObject
+{
+    Q_OBJECT
+public:
+    explicit LogParser(SerialPort* serialPort, QObject *parent = 0);
+
+signals:
+    void logMessageReceived(QString logMessage);
+    void packetReceived(QString packet);
+
+protected slots:
+    void onDataAvailable();
+
+private:
+    void parseReceivedData();
+
+    SerialPort* _serialPort;
+    QQueue<unsigned char>* _receivedDataQueue;
+};
+
+#endif // LOGPARSER_H
