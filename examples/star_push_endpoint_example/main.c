bd20926cf32dc94de107272c7da2de193bbd37d0
Maarten Weyn <maarten.weyn@uantwerpen.be>
2013-10-09 13:46:49 +0200

    changed example channel

--- examples/star_push_endpoint_example/main.c
+++ examples/star_push_endpoint_example/main.c
@@ -1,7 +1,18 @@
 /*
- *  Created on: July 10, 2013
- *  Authors:
- * 		maarten.weyn@uantwerpen.be
+ * (C) Copyright 2013 University of Antwerp (http://www.cosys-lab.be) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     maarten.weyn@uantwerpen.be
  *
  * 	Example code for Star topology, push model
  * 	This is the endpoint example
@@ -26,7 +37,7 @@
 
 
 #define SEND_INTERVAL_MS 2000
-#define SEND_CHANNEL 0x12
+#define SEND_CHANNEL 0x1E
 #define TX_EIRP 10
 
 // Macro which can be removed in production environment
