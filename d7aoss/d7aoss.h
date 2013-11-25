82f48ab8601400c7768c0dcf9b4f3231cd103cd9
Maarten Weyn <maarten.weyn@uantwerpen.be>
2013-11-25 10:26:21 +0100

    including doxygen in first set of files

--- d7aoss/d7aoss.h
+++ d7aoss/d7aoss.h
@@ -1,20 +1,19 @@
-/*
- * (C) Copyright 2013 University of Antwerp (http://www.cosys-lab.be) and others.
+/*! \file d7aoss.h
  *
+ * \copyright (C) Copyright 2013 University of Antwerp (http://www.cosys-lab.be) and others.\n
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
+ * http://www.gnu.org/licenses/lgpl-2.1.html\n
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * Contributors:
- *     maarten.weyn@uantwerpen.be
- *     glenn.ergeerts@uantwerpen.be
+ * \author maarten.weyn@uantwerpen.be
+ * \author glenn.ergeerts@uantwerpen.be
  *
+ *	\brief High Level API for OSS-7
  *
  *  The high level API to be used by applications which use the dash7 stack
  */
@@ -26,10 +25,10 @@
 extern "C" {
 #endif
 
-/*
+/*!
  *  Configuration section
  *
- *  TODO get from configure step or similar
+ *  \todo Get from configure step or similar
  */
 
 #define DEBUG
--- d7aoss/hal/system.h
+++ d7aoss/hal/system.h
@@ -1,20 +1,21 @@
-/*
- * (C) Copyright 2013 University of Antwerp (http://www.cosys-lab.be) and others.
+/*! \file system.h
  *
+ * \copyright (C) Copyright 2013 University of Antwerp (http://www.cosys-lab.be) and others.\n
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
+ * http://www.gnu.org/licenses/lgpl-2.1.html\n
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * Contributors:
- * 		maarten.weyn@uantwerpen.be
- *     	glenn.ergeerts@uantwerpen.be
+ * \author maarten.weyn@uantwerpen.be
+ * \author glenn.ergeerts@uantwerpen.be
  *
+ *	\brief High Level API for hardware
+ *
+ *  The high level API to initialize the hardware
  */
 
 #ifndef __SYSTEM_H__
