9d768aadcf63385c38145512cb450ef58efbfce8
Armin van der Togt <armin@otheruse.nl>
2014-02-13 14:46:38 +0100

    usb name changed

--- d7aoss/hal/stm32l/usbserial/usb_desc.c
+++ d7aoss/hal/stm32l/usbserial/usb_desc.c
@@ -159,7 +159,7 @@ const uint8_t Virtual_Com_Port_StringProduct[VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT
     VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT,          /* bLength */
     USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
     /* Product name: "STM32 Virtual COM Port" */
-    'S', 0, 'T', 0, 'M', 0, '3', 0, '2', 0, ' ', 0, 'V', 0, 'i', 0,
+    'D', 0, 'A', 0, 'S', 0, 'H', 0, '7', 0, ' ', 0, 'V', 0, 'i', 0,
     'r', 0, 't', 0, 'u', 0, 'a', 0, 'l', 0, ' ', 0, 'C', 0, 'O', 0,
     'M', 0, ' ', 0, 'P', 0, 'o', 0, 'r', 0, 't', 0, ' ', 0, ' ', 0
   };
