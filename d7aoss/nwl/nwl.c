d44881a2add1031328dee2d9d5eaf433370dc88b
unknown <maarten.weyn@uantwerpen.be>
2014-06-27 08:01:23 +0200

    implemeting nwl

--- d7aoss/nwl/nwl.c
+++ d7aoss/nwl/nwl.c
@@ -27,7 +27,7 @@ static nwl_tx_callback_t nwl_tx_callback;
 nwl_rx_res_t res;
 static nwl_background_frame_t bf;
 //static nwl_ff_D7ADP_t d7adp_frame;
-static nwl_ff_D7ANP_t d7anp_frame;;
+static nwl_ff_D7ANP_t d7anp_frame;
 
 static void dll_tx_callback(Dll_Tx_Result status)
 {
@@ -43,16 +43,13 @@ static void dll_rx_callback(dll_rx_res_t* result)
 
 	if (result->frame_type == FrameTypeBackgroundFrame)
 	{
-		//bf.tx_eirp = 0;
-		//bf.subnet = result->frame->subnet;
 		bf.bpid = result->frame->payload[0];
-		memcpy((void*) bf.protocol_data,(void*) &(result->frame->payload[1]), 2);
+		bf.data_length = 2; // currently both types are lenght 2
+		bf.protocol_data = &(result->frame->payload[1]);
 
 		res.data = &bf;
 		res.protocol_type = ProtocolTypeBackgroundProtocol;
-
-		//TODO: should a BF be send to transport layer??
-	}
+	} else
 	{
 		d7anp_frame.d7anls_auth_data = NULL;
 		d7anp_frame.d7anls_header = NULL;
@@ -129,16 +126,17 @@ void nwl_build_beaconprotocol_data(uint8_t spectrum_id, int8_t tx_eirp, uint8_t 
 
 	// protocol data
 	// change to MSB
-	queue_push_u8(&tx_queue, virtual_id >> 8);
-	queue_push_u8(&tx_queue, virtual_id & 0XFF);
+	queue_push_u8(&tx_queue, virtual_id[1]);
+	queue_push_u8(&tx_queue, virtual_id[0]);
 
 	nwl_build_background_frame(spectrum_id, tx_eirp, subnet);
 }
 
+/** \copydoc nwl_build_network_protocol_data */
 
-void nwl_build_network_protocol_data(uint8_t* data, uint8_t length, nwl_security* security, nwl_routing_header* routing, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id)
+void nwl_build_network_protocol_data(uint8_t control, nwl_security* security, void* source_access, uint8_t* target_address, uint8_t target_address_lenght, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id)
 {
-	uint8_t offset = 0;
+	uint8_t access_tmpl_length = 0;
 
 	dll_tx_cfg_t dll_params;
 	dll_params.eirp = tx_eirp;
@@ -146,27 +144,53 @@ void nwl_build_network_protocol_data(uint8_t* data, uint8_t length, nwl_security
 	dll_params.subnet = subnet;
 	dll_params.frame_type = FrameTypeForegroundFrame;
 
-	if (security != NULL)
+	if ((security != NULL) || (control & NWL_CONTRL_NLS))
 	{
 		#ifdef LOG_NWL_ENABLED
 		log_print_stack_string(LOG_NWL, "NWL: security not implemented");
 		#endif
-
 	}
 
-	if (routing != NULL)
+	if (control & NWL_CONTRL_CFG(0x0F))
 	{
 		#ifdef LOG_NWL_ENABLED
-		log_print_stack_string(LOG_NWL, "NWL: routing not implemented");
+		log_print_stack_string(LOG_NWL, "NWL: Routing not implemented");
 		#endif
 	}
 
-	queue_clear(&tx_queue);
-	queue_push_u8_array(&tx_queue, data, length);
+	switch(control & NWL_CONTRL_SRC_FULL)
+	{
+		case NWL_CONTRL_SRC_VID:
+			access_tmpl_length = 2;
+			break;
+		case NWL_CONTRL_SRC_VID:
+			access_tmpl_length = 8;
+			break;
+		case NWL_CONTRL_SRC_FULL:
+		{
+			nwl_full_access_template* access = (nwl_full_access_template*) source_access;
+			if (access->control & NWL_ACCESS_TEMPL_CTRL_VID)
+				access_tmpl_length = 4;
+			else
+				access_tmpl_length = 10;
 
-	//TODO: assert dll_data_length < 255-7
-	dll_create_frame( NULL, 0, &dll_params);
+			access_tmpl_length += access->control & OxOF;
+			break;
+		}
+	}
+
+	queue_create_header_space(&tx_queue, 1 + access_tmpl_length);
+
+	tx_queue.front[0] = control;
+
+	if (access_tmpl_length > 0)
+	{
+		memcpy(&tx_queue.front[1], source_access, access_tmpl_length);
+	}
+
+	dll_create_frame(target_address, target_address_lenght, &dll_params);
 }
+
 //
 //void nwl_build_datastream_protocol_data(uint8_t* data, uint8_t length, nwl_security* security, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id)
 //{
--- d7aoss/nwl/nwl.h
+++ d7aoss/nwl/nwl.h
@@ -23,10 +23,21 @@
 #define BPID_AdvP 0xF0
 #define BPID_BeaconP 0xF1
 
+#define NWL_CONTRL_NLS		1 << 7
+#define NWL_CONTRL_EXT		1 << 6
+#define NWL_CONTRL_CFG(VAL)	(0x0F & VAL) << 2
+#define NWL_CONTRL_SRC_NO	0
+#define NWL_CONTRL_SRC_UID	1
+#define NWL_CONTRL_SRC_VID	2
+#define NWL_CONTRL_SRC_FULL	3
+
+#define NWL_ACCESS_TEMPL_CTRL_VID		1 << 6
+#define NWL_ACCESS_TEMPL_CTRL_CSMA(VAL)	(VAL & 0x03) << 4
+#define NWL_ACCESS_TEMPL_CTRL_LEN(VAL)	(VAL & 0x0F)
+
 typedef enum {
 	ProtocolTypeBackgroundProtocol,
-	ProtocolTypeNetworkProtocol,
-	ProtocolTypeDatastreamProtocol
+	ProtocolTypeNetworkProtocol
 } Protocol_Type;
 
 
@@ -43,17 +54,20 @@ typedef struct {
 } nwl_ff_D7ADP_t;
 
 typedef struct {
+	uint8_t control;
 	uint8_t* d7anls_header;
-	uint8_t* d7anp_routing_header;
+	uint8_t* d7anp_source_access_templ;
 	uint8_t payload_length;
 	uint8_t* payload;
 	uint8_t* d7anls_auth_data;
 } nwl_ff_D7ANP_t;
 
 typedef struct {
-	uint16_t eta;
-} AdvP_Data;
-
+	uint8_t control;
+	uint8_t* device_id;
+	uint8_t timeout;
+	uint8_t* channel_list;
+} nwl_full_access_template;
 
 typedef struct
 {
@@ -67,7 +81,6 @@ typedef struct
 typedef struct {
 	uint8_t nls_code;
 	uint8_t* nls_initialization_data;
-	uint8_t* target_address;
 } nwl_security;
 
 typedef struct {
@@ -117,7 +130,26 @@ void nwl_build_advertising_protocol_data(uint16_t eta, uint8_t spectrum_id, int8
 void nwl_build_beaconprotocol_data(uint8_t spectrum_id, int8_t tx_eirp, uint8_t subnet);
 
 // Foreground frames
-void nwl_build_network_protocol_data(uint8_t* data, uint8_t length, nwl_security* security, nwl_routing_header* routing, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id);
+
+void nwl_build_advertising_protocol_data(uint16_t eta, uint8_t spectrum_id, int8_t tx_eirp, uint8_t subnet);
+
+/*! \brief Builds a network protocol foreground frame  (Network Layer)
+ *
+ *  Creates a Foreground Frame for the network protocol
+ *  The payload should be put in the TX queue prior to calling this method
+ *
+ *
+ *  \param uint8_t control 			The control field
+ *  \param nwl_security* security	The optional security header
+ *  \param void* source_access		The optional source access template
+ *  \param uint8_t* target_address  The target address
+ *  \param uint8_t target_address_lenght The size of the target address (0/2/8 bytes)
+ *  \param uint8_t subnet			The subnet of the message
+ *  \param uint8_t spectrum_id 		The channel on which to send the foreground frame.
+ *  \param uint8_t tx_eirp 			The send EIRP.
+ *  \param uint8_t subnet 			The subnet to of the background frame.
+ */
+void nwl_build_network_protocol_data(uint8_t control, nwl_security* security, void* source_access, uint8_t* target_address, uint8_t target_address_lenght, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id);
 //void nwl_build_datastream_protocol_data(uint8_t* data, uint8_t length, nwl_security* security, uint8_t subnet, uint8_t spectrum_id, int8_t tx_eirp, uint8_t dialog_id);
 
 void nwl_rx_start(uint8_t subnet, uint8_t spectrum_id, Protocol_Type type);
