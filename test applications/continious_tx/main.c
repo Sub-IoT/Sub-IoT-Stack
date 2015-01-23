61d69427f9aae1b79be4227c5b2ab39121007d1b
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-01-23 09:53:12 +0100

    continous_tx working on cc1101, uses OOK on 433MHz for now

--- test applications/continious_tx/main.c
+++ test applications/continious_tx/main.c
@@ -6,83 +6,127 @@
 
 
 #include <string.h>
+#include <stdint.h>
+
+#include <d7aoss.h>
 
 #include <phy/phy.h>
 
 #include <hal/system.h>
 #include <hal/leds.h>
 
-#include <msp430.h>
+#include <framework/log.h>
 
-#define PACKET_LEN 19
-#define INTERRUPT_BUTTON1       (1)
-#define CHANNEL_COUNT   8
+#include "../../d7aoss/phy/cc1101/cc1101_constants.h"
+#include "../../d7aoss/phy/cc1101/cc1101_phy.h"
 
-static uint8_t packet[PACKET_LEN] = { 0x13, 0x50, 0xF1, 0x20, 0x59, 0x40, 0x46, 0x93, 0x21, 0xAB, 0x00, 0x31, 0x00, 0x24, 0x00, 0x00, 0x00, 0x01, 0x01 };
-
-static uint8_t spectrum_ids[CHANNEL_COUNT] = { 0x10, 0x12, 0x14, 0x16, 0x18, 0x1A, 0x1C, 0x1E }; // TODO only normal channel classes?
-
-static uint8_t current_spectrum_index = 0;
-
-static uint8_t interrupt_flags = 0;
-
-phy_tx_cfg_t tx_cfg = {
-	0x10,
-	0,
-	0,
-	PACKET_LEN,
-	packet
+// Address config = No address check
+// Sync word qualifier mode = No preamble/sync
+// PA ramping = false
+// Base frequency = 433.999573
+// Data rate = 38.3835
+// RX filter BW = 101.562500
+// Packet length = 255
+// Manchester enable = false
+// Packet length mode = Infinite packet length mode
+// CRC enable = false
+// TX power = 0
+// Modulation format = ASK/OOK
+// Device address = 0
+// Whitening = false
+// Channel spacing = 199.951172
+// Data format = Random mode
+// Deviation = 20.629883
+// Preamble count = 4
+// CRC autoflush = false
+// Carrier frequency = 433.999573
+// Modulated = false
+// Channel number = 0
+// Rf settings for CC1101
+RF_SETTINGS rfSettings = {
+    0x0B,  // IOCFG2              GDO2 Output Pin Configuration
+    0x2E,  // IOCFG1              GDO1 Output Pin Configuration
+    0x0C,  // IOCFG0              GDO0 Output Pin Configuration
+    0x47,  // FIFOTHR             RX FIFO and TX FIFO Thresholds
+    0xD3,  // SYNC1               Sync Word, High Byte
+    0x91,  // SYNC0               Sync Word, Low Byte
+    0xFF,  // PKTLEN              Packet Length
+    0x04,  // PKTCTRL1            Packet Automation Control
+    0x22,  // PKTCTRL0            Packet Automation Control
+    0x00,  // ADDR                Device Address
+    0x00,  // CHANNR              Channel Number
+    0x08,  // FSCTRL1             Frequency Synthesizer Control
+    0x00,  // FSCTRL0             Frequency Synthesizer Control
+    0x10,  // FREQ2               Frequency Control Word, High Byte
+    0xB1,  // FREQ1               Frequency Control Word, Middle Byte
+    0x3A,  // FREQ0               Frequency Control Word, Low Byte
+    0xCA,  // MDMCFG4             Modem Configuration
+    0x83,  // MDMCFG3             Modem Configuration
+    0xB0,  // MDMCFG2             Modem Configuration
+    0x22,  // MDMCFG1             Modem Configuration
+    0xF8,  // MDMCFG0             Modem Configuration
+    0x35,  // DEVIATN             Modem Deviation Setting
+    0x07,  // MCSM2               Main Radio Control State Machine Configuration
+    0x30,  // MCSM1               Main Radio Control State Machine Configuration
+    0x18,  // MCSM0               Main Radio Control State Machine Configuration
+    0x16,  // FOCCFG              Frequency Offset Compensation Configuration
+    0x6C,  // BSCFG               Bit Synchronization Configuration
+    0x43,  // AGCCTRL2            AGC Control
+    0x40,  // AGCCTRL1            AGC Control
+    0x91,  // AGCCTRL0            AGC Control
+    0x87,  // WOREVT1             High Byte Event0 Timeout
+    0x6B,  // WOREVT0             Low Byte Event0 Timeout
+    0xFB,  // WORCTRL             Wake On Radio Control
+    0x56,  // FREND1              Front End RX Configuration
+    0x11,  // FREND0              Front End TX Configuration
+    0xE9,  // FSCAL3              Frequency Synthesizer Calibration
+    0x2A,  // FSCAL2              Frequency Synthesizer Calibration
+    0x00,  // FSCAL1              Frequency Synthesizer Calibration
+    0x1F,  // FSCAL0              Frequency Synthesizer Calibration
+    0x41,  // RCCTRL1             RC Oscillator Configuration
+    0x00,  // RCCTRL0             RC Oscillator Configuration
+    0x59,  // FSTEST              Frequency Synthesizer Calibration Control
+    0x7F,  // PTEST               Production Test
+    0x3F,  // AGCTEST             AGC Test
+    0x81,  // TEST2               Various Test Settings
+    0x35,  // TEST1               Various Test Settings
+    0x09,  // TEST0               Various Test Settings
+    0x00,  // PARTNUM             Chip ID
+    0x04,  // VERSION             Chip ID
+    0x00,  // FREQEST             Frequency Offset Estimate from Demodulator
+    0x00,  // LQI                 Demodulator Estimate for Link Quality
+    0x80,  // RSSI                Received Signal Strength Indication
+    0x01,  // MARCSTATE           Main Radio Control State Machine State
+    0x00,  // WORTIME1            High Byte of WOR Time
+    0x00,  // WORTIME0            Low Byte of WOR Time
+    0x00,  // PKTSTATUS           Current GDOx Status and Packet Status
+    0x94,  // VCO_VC_DAC          Current Setting from PLL Calibration Module
+    0x00,  // TXBYTES             Underflow and Number of Bytes
+    0x00,  // RXBYTES             Overflow and Number of Bytes
+    0x00,  // RCCTRL1_STATUS      Last RC Oscillator Calibration Result
+    0x00,  // RCCTRL0_STATUS      Last RC Oscillator Calibration Result
 };
 
-uint8_t get_next_spectrum_id()
+uint8_t buffer[256];
+
+void main()
 {
-        current_spectrum_index++;
-        if(current_spectrum_index == CHANNEL_COUNT)
-                current_spectrum_index = 0;
+	system_init(buffer, sizeof(buffer), buffer, sizeof(buffer));
+	spi_init();
 
-        return spectrum_ids[current_spectrum_index];
-}
+	int p = ReadSingleReg(PARTNUM);
+	log_print_string("PARTNUM 0x%x", p);
+	p = ReadSingleReg(VERSION);
+	log_print_string("VERSION 0x%x", p);
+	log_print_string("started");
 
-void main(void) {
-        system_init();
-        button_enable_interrupts();
+	WriteRfSettings(&rfSettings);
+	Strobe(RF_SFTX);
 
-        phy_init();
+	//uint8_t spectrum_id[2] = { 4, 0 };
+	//phy_translate_and_set_settings(spectrum_id, 0);
 
-        while(1)
-        {
-        	if(!phy_is_tx_in_progress())
-        	{
-                led_toggle(3);
-                phy_tx(&tx_cfg);
-        	}
+	Strobe(RF_STX);
 
-			if(INTERRUPT_BUTTON1 & interrupt_flags)
-			{
-				interrupt_flags &= ~INTERRUPT_BUTTON1;
-
-				tx_cfg.spectrum_id = get_next_spectrum_id();
-
-				button_clear_interrupt_flag();
-				button_enable_interrupts();
-			}
-
-			system_lowpower_mode(4, 1);
-        }
-}
-
-#pragma vector=PORT1_VECTOR
-__interrupt void PORT1_ISR (void)
-{
-        if(button_is_active(1))
-                interrupt_flags |= INTERRUPT_BUTTON1;
-        else
-                interrupt_flags &= ~INTERRUPT_BUTTON1;
-
-        if(interrupt_flags != 0)
-        {
-                button_disable_interrupts();
-                // TODO: debouncing
-                LPM4_EXIT;
-        }
+	while(1);
 }
