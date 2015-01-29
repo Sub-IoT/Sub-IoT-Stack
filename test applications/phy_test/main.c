d9a7c9d017882b10d2655b7ec854eaf684aca339
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-01-29 11:28:26 +0100

    phy_test works again

--- test applications/phy_test/main.c
+++ test applications/phy_test/main.c
@@ -17,27 +17,24 @@
 #include <hal/system.h>
 #include <framework/log.h>
 
-#include "interrupts.h"
+#define RX_MODE
 
-static uint8_t rtcEnabled = 0;
-
-static uint8_t buffer[16] = {0x10, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+static uint8_t tx_data[16] = {16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 /*
  */
 phy_tx_cfg_t tx_cfg = {
-	.spectrum_id=0x90,
+    .spectrum_id={ 0x00, 0x00}, // TODO
 	.sync_word_class=1,
-	.eirp=0,
-	.length=16,
-	.data=buffer
+    .eirp=0
 };
 
 phy_rx_cfg_t rx_cfg = {
-	.spectrum_id=0x90,
+	.spectrum_id={ 0x00, 0x00}, // TODO
 	.sync_word_class=1,
 	.length=0,
-	.timeout=0
+	.timeout=1000,
+	.scan_minimum_energy = -140
 };
 
 void start_rx(void)
@@ -53,79 +50,30 @@ void start_tx(void)
 
 void rx_callback(phy_rx_data_t* rx_data)
 {
-	if(memcmp(buffer, rx_data->data, sizeof(buffer)) == 0)
-		led_toggle(1);
+//	if(memcmp(buffer, rx_data->data, sizeof(buffer)) == 0)
+	led_toggle(1);
+	start_rx();
 }
+
+uint8_t tx_buffer[128];
+uint8_t rx_buffer[128];
+
 
 int main(void)
 {
-	system_init();
-    rtc_init_counter_mode();
-    rtc_start();
-
-	//test_fec_encoding();
-	//test_fec_decoding();
-
-	button_enable_interrupts();
-
+    system_init(tx_buffer, sizeof(tx_buffer), rx_buffer, sizeof(rx_buffer));
 
 	phy_init();
 	phy_set_rx_callback(rx_callback);
 
-	while(1) {
-        if (INTERRUPT_BUTTON1 & interrupt_flags) {
-        	interrupt_flags &= ~INTERRUPT_BUTTON1;
-//        	led_toggle(1);
+	#ifdef RX_MODE
+		start_rx();
+		system_lowpower_mode(4,1);
+	#else
+		queue_push_u8_array(&tx_queue, tx_data, sizeof(tx_data));
+		start_tx();
+	#endif
 
-        	start_tx();
-
-        	button_clear_interrupt_flag();
-        	button_enable_interrupts();
-        }
-
-        if (INTERRUPT_BUTTON3 & interrupt_flags) {
-			interrupt_flags &= ~INTERRUPT_BUTTON3;
-
-			if (rtcEnabled) {
-				rtcEnabled = 0;
-				rtc_disable_interrupt();
-			} else {
-				rtcEnabled = 1;
-				rtc_enable_interrupt();
-			}
-
-			button_clear_interrupt_flag();
-			button_enable_interrupts();
-        }
-
-        if (INTERRUPT_RTC & interrupt_flags) {
-        	interrupt_flags &= ~INTERRUPT_RTC;
-        	led_toggle(0);
-
-        	start_tx();
-        }
-
-        if (!phy_is_rx_in_progress() && !phy_is_tx_in_progress()) {
-        	start_rx();
-        }
-
-        if (phy_is_rx_in_progress())
-        {
-        	led_on(2);
-        } else {
-        	led_off(2);
-        }
-
-        if (phy_is_tx_in_progress())
-        {
-        	led_on(3);
-        } else {
-        	led_off(3);
-        }
-
-
-//		system_lowpower_mode(4,1);
-	}
 	return 0;
 }
 
