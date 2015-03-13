15e410ce2ef395cca6162996f382fa7f3287bf8e
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-03-13 16:11:00 +0100

    switched to TIMER0_A1_VECTOR instead of TIMER_A0_VECTOR, which is called for overflow interrupt as well (instead of only for compare interrupt). Overflow still needs to be tested

--- stack/framework/hal/chips/cc430/cc430_timer.c
+++ stack/framework/hal/chips/cc430/cc430_timer.c
@@ -1,6 +1,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "hwtimer.h"
 #include "hwatomic.h"
@@ -8,56 +9,68 @@
 #include "msp430.h"
 #include "timer_a.h"
 
+#define CC430_TIMER_BASE_ADDRESS TIMER_A0_BASE
+#define CC430_TIMER_COMPARE_REGISTER TIMER_A_CAPTURECOMPARE_REGISTER_1
+
 static timer_callback_t compare_f = 0x0;
 static timer_callback_t overflow_f = 0x0;
 static bool timer_inited = false;
 
 // Timer A0 interrupt service routine
-void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) TIMER0_A0_VECTOR_ISR (void)
+void __attribute__ ((interrupt(TIMER0_A1_VECTOR))) TIMER0_A1_VECTOR_ISR (void)
 {
-    uint16_t ta0cctl0 = TA0CCTL0; // TODO tmp
-    uint16_t ta0iv = TA0IV; // TODO tmp
-    uint16_t ta0r = TA0R; // TODO tmp
-    uint16_t interrupt_flags = Timer_A_getCaptureCompareInterruptStatus(
-                TIMER_A0_BASE,
-                TIMER_A_CAPTURECOMPARE_REGISTER_0, // TODO def
-                TIMER_A_CAPTURE_OVERFLOW | TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG);
-    //if((interrupt_flags & TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG) == TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG)
+    switch(TA0IV)
     {
-        Timer_A_disableCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        if(compare_f != 0x0)
-            compare_f();
-    }
-    // TODO check overflow
+        case 0x0E:
+            // timer overflowed
+            overflow_f();
+            break;
+    	case 0x02:
+            // compare value for register 1 reached
+            Timer_A_disableCaptureCompareInterrupt(CC430_TIMER_BASE_ADDRESS, CC430_TIMER_COMPARE_REGISTER);
+            if(compare_f != 0x0)
+                compare_f();
 
-    Timer_A_clearTimerInterruptFlag(TIMER_A0_BASE);
-    __bic_SR_register_on_exit(CPUOFF);//(LPM0_bits); // go back to active mode to re-enter scheduler // TODO detect which LPM we where in before?
+            break;
+        default:
+            assert(false);
+    }
+
+    Timer_A_clearTimerInterruptFlag(CC430_TIMER_BASE_ADDRESS);
+    __bic_SR_register_on_exit(CPUOFF); // go back to active mode to re-enter scheduler
 }
 
 error_t hw_timer_init(hwtimer_id_t timer_id, uint8_t frequency, timer_callback_t compare_callback, timer_callback_t overflow_callback)
 {
     // TODO check id
-    compare_f = compare_callback;
-    overflow_f = overflow_callback;
-    timer_inited = true;
 
     Timer_A_initContinuousModeParam param =
     {
         .clockSource = TIMER_A_CLOCKSOURCE_ACLK,
         .clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_8, // ACLK = REFO = 32768 Hz => /8 = 1024 Hz = 1 tick
-        .timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE,
+        .timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE,
         .timerClear = TIMER_A_DO_CLEAR,
         .startTimer = true
     };
 
-    Timer_A_initContinuousMode(TIMER_A0_BASE, &param);
+    start_atomic();
+    {
+        compare_f = compare_callback;
+        overflow_f = overflow_callback;
+        timer_inited = true;
+
+    	Timer_A_clearTimerInterruptFlag(CC430_TIMER_BASE_ADDRESS);
+    	Timer_A_initContinuousMode(CC430_TIMER_BASE_ADDRESS, &param);
+    }
+    end_atomic();
+
     return SUCCESS;
 }
 
 hwtimer_tick_t hw_timer_getvalue(hwtimer_id_t timer_id)
 {
     // TODO check id + inited
-    return Timer_A_getCounterValue(TIMER_A0_BASE);
+    return Timer_A_getCounterValue(CC430_TIMER_BASE_ADDRESS);
 }
 
 error_t hw_timer_schedule(hwtimer_id_t timer_id, hwtimer_tick_t tick )
@@ -65,16 +78,16 @@ error_t hw_timer_schedule(hwtimer_id_t timer_id, hwtimer_tick_t tick )
     // TODO check id + inited
     Timer_A_initCompareModeParam param =
     {
-        .compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_0,
+        .compareRegister = CC430_TIMER_COMPARE_REGISTER,
         .compareInterruptEnable = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,
-        .compareOutputMode = TIMER_A_OUTPUTMODE_SET, // TODO
+        .compareOutputMode = TIMER_A_OUTPUTMODE_SET,
         .compareValue = tick
     };
 
     start_atomic();
     {
-        Timer_A_initCompareMode(TIMER_A0_BASE, &param);
-        Timer_A_clearTimerInterruptFlag(TIMER_A0_BASE);
+        Timer_A_initCompareMode(CC430_TIMER_BASE_ADDRESS, &param);
+        Timer_A_clearTimerInterruptFlag(CC430_TIMER_BASE_ADDRESS);
     }
     end_atomic();
 }
@@ -84,8 +97,8 @@ error_t hw_timer_cancel(hwtimer_id_t timer_id)
     // TODO check id + inited
     start_atomic();
     {
-        Timer_A_disableInterrupt(TIMER_A0_BASE);
-        Timer_A_clearTimerInterruptFlag(TIMER_A0_BASE);
+        Timer_A_disableInterrupt(CC430_TIMER_BASE_ADDRESS);
+        Timer_A_clearTimerInterruptFlag(CC430_TIMER_BASE_ADDRESS);
     }
     end_atomic();
 }
@@ -95,7 +108,7 @@ error_t hw_timer_counter_reset(hwtimer_id_t timer_id)
     // TODO check id + inited
     start_atomic();
     {
-        Timer_A_clearTimerInterruptFlag(TIMER_A0_BASE);
+        Timer_A_clearTimerInterruptFlag(CC430_TIMER_BASE_ADDRESS);
         TA0R = 0;
     }
     end_atomic();
