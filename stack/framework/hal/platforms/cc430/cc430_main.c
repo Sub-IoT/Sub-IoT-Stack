3b3be230b42dc187fdfad641113e2443f60cdcd7
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-04-09 12:06:47 +0200

    cleaned cc430_main.c

--- stack/framework/hal/platforms/cc430/cc430_main.c
+++ stack/framework/hal/platforms/cc430/cc430_main.c
@@ -29,29 +29,19 @@
 
 void __platform_init()
 {
-    // TODO
     __cc430_mcu_init();
     __gpio_init();
-//    __uart_init();
+//    __uart_init(); // TODO
     __led_init();
 
-#ifdef USE_CC1101
-    //TODO: add calls to hw_gpio_configure_pin for the pins used by the CC1101 driver
-    //(and possibly the spi interface)
-#endif
-//    __hw_debug_init();
-
-    error_t err;
-//    err = hw_gpio_configure_pin(BUTTON0, true, gpioModeInput,0); assert(err == SUCCESS);
-//    err = hw_gpio_configure_pin(BUTTON1, true, gpioModeInput,0); assert(err == SUCCESS);
+//    __hw_debug_init(); // TODO
 }
 
 void __platform_post_framework_init()
 {
-    //__ubutton_init();
+    //__ubutton_init(); // TODO
 }
 
-// TODO is main() platform dependent or can this be generic? (if generic, move somewhere else)
 int main()
 {
     //initialise the platform itself
