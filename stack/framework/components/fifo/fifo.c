945ee681b6fa97d8f938206eb3dafa8667511973
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-07-04 21:09:28 +0200

    added a generic fifo module (providing circular buffer)

---
+++ stack/framework/components/fifo/fifo.c
@@ -0,0 +1,104 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file fifo.c
+ * \author glenn.ergeerts@uantwerpen.be
+ */
+
+#include "fifo.h"
+#include "string.h"
+#include "errors.h"
+
+void fifo_init(fifo_t *fifo, uint8_t *buffer, uint16_t max_size)
+{
+    fifo->buffer = buffer;
+    fifo->head_idx = 0;
+    fifo->tail_idx = 0;
+    fifo->max_size = max_size;
+}
+
+error_t fifo_put(fifo_t *fifo, uint8_t *data, uint16_t len)
+{
+    if(fifo->tail_idx + len <= fifo->max_size)
+    {
+        memcpy(fifo->buffer + fifo->tail_idx, data, len);
+        fifo->tail_idx += len;
+        return SUCCESS;
+    }
+
+    uint16_t space_left_before_max_size = fifo->max_size - fifo->tail_idx;
+    uint16_t space_needed_after_wrap = len - space_left_before_max_size;
+    if(fifo->head_idx > space_needed_after_wrap)
+    {
+        // wrap
+        memcpy(fifo->buffer + fifo->tail_idx, data, space_left_before_max_size);
+        memcpy(fifo->buffer, data + space_left_before_max_size, space_needed_after_wrap);
+        fifo->tail_idx = space_needed_after_wrap;
+        return SUCCESS;
+    }
+    else
+        return ESIZE;
+}
+
+error_t fifo_pop(fifo_t* fifo, uint8_t* buffer, uint16_t len)
+{
+    if(len > fifo_get_size(fifo)) return ESIZE;
+
+    if(fifo->head_idx + len < fifo->max_size)
+    {
+        memcpy(buffer, fifo->buffer + fifo->head_idx, len);
+        fifo->head_idx += len;
+        return SUCCESS;
+    }
+
+    // wrap
+    uint16_t len_until_max_size = fifo->max_size - fifo->head_idx;
+    uint16_t len_after_wrap = len - len_until_max_size;
+    memcpy(buffer, fifo->buffer + fifo->head_idx, len_until_max_size);
+    memcpy(buffer + len_until_max_size, fifo->buffer, len_after_wrap);
+    fifo->head_idx = len_after_wrap;
+    return SUCCESS;
+}
+
+error_t fifo_peek(fifo_t* fifo, uint8_t* buffer, uint16_t offset, uint16_t len)
+{
+    if(len > fifo_get_size(fifo)) return ESIZE;
+
+    uint16_t start_idx = fifo->head_idx + offset;
+    if(fifo->head_idx + offset + len < fifo->max_size)
+    {
+        memcpy(buffer, fifo->buffer + start_idx, len);
+        return SUCCESS;
+    }
+
+    if(start_idx > fifo->max_size)
+        start_idx -= fifo->max_size;
+
+    uint16_t space_before_max_size = fifo->max_size - start_idx;
+    memcpy(buffer, fifo->buffer + start_idx, space_before_max_size);
+    memcpy(buffer + space_before_max_size, fifo->buffer, len - space_before_max_size);
+    return SUCCESS;
+}
+
+uint16_t fifo_get_size(fifo_t* fifo)
+{
+    if(fifo->head_idx <= fifo->tail_idx)
+        return fifo->tail_idx - fifo->head_idx;
+    else
+        return fifo->tail_idx + (fifo->max_size - fifo->head_idx);
+}
---
+++ stack/framework/inc/fifo.h
@@ -0,0 +1,97 @@
+
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file fifo.h
+ * @addtogroup fifo
+ * @ingroup framework
+ * @{
+ * @brief A generic FIFO implementation which allows pushing and popping bytes in a circular buffer.
+ *
+ */
+
+#ifndef FIFO_H
+#define FIFO_H
+
+#include "types.h"
+
+/**
+ * @brief This struct contains the FIFO state variables
+ *
+ * A pointer to this is passed to alle functions of the fifo module
+ **/
+typedef struct {
+    uint16_t head_idx;      /**< The offset in buffer to the headof the FIFO */
+    uint16_t tail_idx;      /**< The offset in buffer to the headof the FIFO */
+    uint16_t max_size;      /**< The maximum size of bytes contained in the FIFO */
+    uint8_t* buffer;        /**< The buffer where the data is stored*/
+} fifo_t;
+
+/**
+ * @brief Initializes the fifo.
+ * @param fifo          Fifo state, initialized by this function
+ * @param buffer        The buffer used for the fifo, the caller is responsible for allocating this to be big enough for max_size
+ * @param max_size      The maximum size of bytes contained in the FIFO
+ */
+void fifo_init(fifo_t* fifo, uint8_t* buffer, uint16_t max_size);
+
+/**
+ * @brief Put bytes in tothe FIFO
+ * @param fifo  Pointer to the fifo object
+ * @param data  Pointer to the data to be put in the FIFO
+ * @param len   Number of bytes to put in the FIFO
+ * @returns SUCCESS or ESIZE when data would overwrite head of FIFO
+ */
+error_t fifo_put(fifo_t* fifo, uint8_t* data, uint16_t len);
+
+/**
+ * @brief Peek at the FIFO contents without popping. Fills buffer with the data in the FIFO starting from head_idx + offset for len bytes
+ * @param fifo      Pointer to the fifo object
+ * @param buffer    buffer to be filled
+ * @param offset    offset starting from head
+ * @param len       length in number of bytes to read
+ * @returns SUCCESS or ESIZE when len > max_size or len > current size
+ */
+error_t fifo_peek(fifo_t* fifo, uint8_t* buffer, uint16_t offset, uint16_t len);
+
+/**
+ * @brief Read and pop bytes from the FIFO
+ * @param fifo      Pointer to the fifo object
+ * @param buffer    Pointer to buffer where the first len bytes of FIFO can be copied to. Caller is responsible for allocating this buffer.
+ * @param len       number of bytes to read/pop
+ * @returns SUCCESS or ESIZE if len > current size or when FIFO empty
+ */
+error_t fifo_pop(fifo_t* fifo, uint8_t* buffer, uint16_t len);
+
+/**
+ * @brief Clears the FIFO
+* @param fifo      Pointer to the fifo object
+ */
+void fifo_clear(fifo_t* fifo);
+
+/**
+ * @brief Returns the number of bytes currently in the FIFO
+ * @param fifo      Pointer to the fifo object
+ * @return Number of bytes currently in the FIFO
+ */
+uint16_t fifo_get_size(fifo_t* fifo);
+
+#endif // FIFO_H
+
+/** @}*/
