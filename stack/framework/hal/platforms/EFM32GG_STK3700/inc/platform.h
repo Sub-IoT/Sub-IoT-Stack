65a83947577ae8be2f597197e07e853fe6214705
Maarten Weyn <maarten.weyn@uantwerpen.be>
2015-07-10 11:25:02 +0200

    fixed error in pinout

--- stack/framework/hal/platforms/EFM32GG_STK3700/inc/platform.h
+++ stack/framework/hal/platforms/EFM32GG_STK3700/inc/platform.h
@@ -108,8 +108,8 @@
 
 // CC1101 PIN definitions
 #ifdef USE_CC1101
-    #define CC1101_GDO0_PIN C0
-    #define CC1101_GDO2_PIN C1
+    #define CC1101_GDO0_PIN C3
+    #define CC1101_GDO2_PIN C4
 #endif
 
 #define HAS_LCD
