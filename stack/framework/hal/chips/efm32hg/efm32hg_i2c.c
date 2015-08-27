766a359bbdd27ac4e828d19ed7f4d3f631a3349f
Maarten Weyn <maarten.weyn@uantwerpen.be>
2015-08-27 14:59:30 +0200

    added i2c interface

--- stack/framework/hal/chips/efm32hg/efm32hg_i2c.c
+++ stack/framework/hal/chips/efm32hg/efm32hg_i2c.c
@@ -8,125 +8,82 @@
 #include "em_i2c.h"
 #include <assert.h>
 #include "hwgpio.h"
-#include "MicroOLED.h"
 
 
 #include "hwi2c.h"
 #include "platform.h"
 
+void i2c_master_init(I2C_Init_T* init) {
+	int i;
+	CMU_Clock_TypeDef i2cClock;
+	I2C_Init_TypeDef i2cInit;
 
-#define I2C_SDA D6 // PE12
-#define I2C_SCL D7 // PE13
-#define	I2C_LOC	1
+	EFM_ASSERT(init != NULL);
 
+	CMU_ClockEnable(cmuClock_HFPER, true);
 
-#define I2C_PORT    gpioPortE // PORTE - I2C0 Location #6
-#define I2C_SDA_PIN 12 // PE12
-#define I2C_SCL_PIN 13 // PE13
+	/* Select I2C peripheral clock */
+	if (false)
+	{
+	#if defined( I2C0 )
+	}
+	else if (init->port == I2C0)
+	{
+	i2cClock = cmuClock_I2C0;
+	#endif
+	#if defined( I2C1 )
+	}
+	else if (init->port == I2C1)
+	{
+	i2cClock = cmuClock_I2C1;
+	#endif
+	}
+	else
+	{
+	/* I2C clock is not defined */
+	EFM_ASSERT(false);
+	return;
+	}
+	CMU_ClockEnable(i2cClock, true);
 
-#define LEUART_PORT   gpioPortF // PORTF - LEUART0 Location #3
-#define LEUART_TX_PIN 0 // PF0
-#define LEUART_RX_PIN 1 // PF1
+	/* Output value must be set to 1 to not drive lines low. Set
+	 SCL first, to ensure it is high before changing SDA. */
+	GPIO_PinModeSet(init->sclPort, init->sclPin, gpioModeWiredAndPullUp, 1);
+	GPIO_PinModeSet(init->sdaPort, init->sdaPin, gpioModeWiredAndPullUp, 1);
 
-#define SENSOR_SLAVE_ADDR 0xAC // see TSL2569 datasheet for details
+	/* In some situations, after a reset during an I2C transfer, the slave
+	 device may be left in an unknown state. Send 9 clock pulses to
+	 set slave in a defined state. */
+	for (i = 0; i < 9; i++)
+	{
+	GPIO_PinOutSet(init->sclPort, init->sclPin);
+	GPIO_PinOutClear(init->sclPort, init->sclPin);
+	}
 
-void print_byte_setup(uint8_t byte);
+	/* Enable pins and set location */
+	init->port->ROUTE = I2C_ROUTE_SDAPEN |
+					  I2C_ROUTE_SCLPEN |
+					  (init->portLocation << _I2C_ROUTE_LOCATION_SHIFT);
 
-/* Global variables */
-volatile uint8_t print_byte_array[4] = { 0x30, 0x78, 0x20, 0x20 }; // array for displaying bytes one nibble at a time
-const char header[] = "\n\rEFM32 Zero Gecko I2C Example\n\r";
-const char id_string[] = "Part# RevID: ";
-const char nack_error_string[] = "NACK Received\n\r";
+	/* Set emlib init parameters */
+	i2cInit.enable = true;
+	i2cInit.master = true; /* master mode only */
+	i2cInit.freq = init->i2cMaxFreq;
+	i2cInit.refFreq = init->i2cRefFreq;
+	i2cInit.clhr = init->i2cClhr;
 
-void i2c_init() {
-
-//  CHIP_Init();
-
-
-  /* Setup clock tree */
-  /* Use 14MHz RC oscillator as HF clock source */
-  /* Bootloader Clock Correction */
-  // If using uart bootloader to program, the system boots with 21MHz internal RC as
-  // the HF clock source. Change to 14MHz, but don't overwrite reset tuning value.
-  CMU_HFRCOBandSet(cmuHFRCOBand_14MHz); // set HFCLK to 14MHz
-  /* End Bootloader Clock Correction */
-
-  CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_CORELEDIV2);           // select HFCORECLK/2 as clock source to LFB
-  CMU_ClockEnable(cmuClock_CORELE, true);                           // enable the Low Energy Peripheral Interface clock
-
-  /* LEUART reset (if using bootloader to program)
-  LEUART0->ROUTE = 0;                                               // reset the LEUART route register*/
-  CMU_ClockEnable(cmuClock_LEUART0, false);                         // disable LEUART peripheral clock
-  /*for(i=0; i<2000; i++);                                            // brief delay to keep TX line low at least one frame period
-  /* End LEUART reset */
-
-  //CMU_ClockEnable(cmuClock_LEUART0, true);                          // enable LEUART peripheral clock
-  //CMU_ClockDivSet(cmuClock_LFB, 1);                                 // set LFB prescaler to /2: LEUART clock freq = 14MHz/2/2 = 3.5MHz
-  CMU_ClockEnable(cmuClock_HFPER, true);                            // enable HF peripheral clock  -->> enables fast mode
-  CMU_ClockEnable(cmuClock_GPIO, true);                             // enable GPIO peripheral clock*/
-  CMU_ClockEnable(cmuClock_I2C0, true);                             // enable I2C0 peripheral clock
-
-  /* Configure GPIO */
-  error_t err;
-  //err = hw_gpio_configure_pin(LEUART_PORT, LEUART_TX_PIN, gpioModePushPull, 1); // configure UART TX pin as output, initialize high
-  //err = hw_gpio_configure_pin(LEUART_PORT, LEUART_RX_PIN, gpioModeInput, 0);    // configure UART RX pin as input, no filter
- // err = hw_gpio_configure_pin(I2C_SDA, false, gpioModeWiredAnd, 0); assert(err == SUCCESS);     // configure SDA pin as open drain output
- // err = hw_gpio_configure_pin(I2C_SCL, false, gpioModeWiredAnd, 0); assert(err == SUCCESS);     // configure SCL pin as open drain outputµ
-
-  err = hw_gpio_configure_pin(I2C_SDA, false, gpioModeWiredAnd, 0); assert(err == SUCCESS);     // configure SDA pin as open drain output
-  err = hw_gpio_configure_pin(I2C_SCL, false, gpioModeWiredAnd, 0); assert(err == SUCCESS);     // configure SCL pin as open drain output
-
-  err = hw_gpio_configure_pin(D5, false, gpioModePushPull, 0);
-
-  /* Configure COM Port (LEUART0 asynch)
-  LEUART_Init_TypeDef leuart_init =
-  {
-    .enable   = leuartEnable,                    // enable Receiver and Transmitter
-    .refFreq  = 0,                               // measure reference clock
-    .baudrate = 115200,                          // 115200 bps
-    .databits = leuartDatabits8,                 // 8 data bits
-    .parity   = leuartNoParity,                  // no parity bits
-    .stopbits = leuartStopbits1,                 // 1 stop bit
-  };
-  LEUART_IntClear(LEUART0, (0xFF << 3) | 0x1);   // clear interrupt flags (optional)
-  LEUART_Init(LEUART0, &leuart_init);            // apply configuration to LEUART0
-  LEUART0->ROUTE = (3 << 8) | 0x3;               // use Location #3 (PF0, PF1) enable TX/RX pins
-
-  /* Configure I2C - Fast mode (400kHz) Master */
-  I2C_Init_TypeDef i2c_init =
-  {
-    .enable = true,                              // enable I2C
-    .master = true,                              // I2C master
-    .refFreq = 0,                                // measure reference clock
-    .freq = 400000,                              // 400kbps fast mode
-    .clhr = i2cClockHLRAsymetric,                // use 6:3 bit ratio
-  };
-  I2C_Init(I2C0, &i2c_init);                     // apply configuration to I2C0
-  I2C0->CTRL |= (1 << 2);                        // enable AUTO-ACK feature
-  I2C0->ROUTE = (I2C_LOC << 8) | 0x3;                  // use location #6 (PE11, PE12), enable SDA and SCL
-
-  /* Print Header */
-  //for(i=0; i<sizeof(header); i++) {
-    //LEUART_Tx(LEUART0, header[i]);
-  //}
-
-
-  //while(1);
+	I2C_Init(init->port, &i2cInit);
 }
 
 void i2c_write(uint8_t address,uint8_t* tx_buffer,int length)
 {
-	//uint8_t tx_buffer[17]= {0x03,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0xDF,0xBE};           // software tx buffer
-	//uint8_t rx_buffer[7];           // software rx buffer
-	I2C_TransferReturn_TypeDef ret; // I2C state tracker
-	/* Send power-up command to sensor (see TSL2569 datasheet for details) */
-	//tx_buffer[] = {0xAC,0x02,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0x35,0xC0};
+	I2C_TransferReturn_TypeDef ret;
 	I2C_TransferSeq_TypeDef sensor_message =
 	{
-	    .addr = address,//SENSOR_SLAVE_ADDR,//(SENSOR_SLAVE_ADDR << 1),            // set sensor slave address
-	    .flags = I2C_FLAG_WRITE,//I2C_FLAG_WRITE,                     // indicate basic write
-	    .buf[0].data = tx_buffer,                    // point to tx_buffer
-	    .buf[0].len = length,//15,                             // specify number of bytes
+	    .addr = address,
+	    .flags = I2C_FLAG_WRITE,
+	    .buf[0].data = tx_buffer,
+	    .buf[0].len = length,
 	};
 
 	ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write transaction with sensor
@@ -135,16 +92,15 @@ void i2c_write(uint8_t address,uint8_t* tx_buffer,int length)
 	}
 }
 
-void i2c_read(uint8_t address,uint8_t* rx_buffer, int length)
+void i2c_read(uint8_t address, uint8_t* rx_buffer, int length)
 {
-	//static uint8_t rx_buffer[20]; //fixed size for now
-	I2C_TransferReturn_TypeDef ret; // I2C state tracker
+	I2C_TransferReturn_TypeDef ret;
 	I2C_TransferSeq_TypeDef sensor_message =
 	{
-		.addr = SENSOR_SLAVE_ADDR,//(SENSOR_SLAVE_ADDR << 1),            // set sensor slave address
-		.flags = I2C_FLAG_READ,//I2C_FLAG_WRITE,                     // indicate basic write
-		.buf[1].data = rx_buffer,                    // point to tx_buffer
-		.buf[1].len = length,//15,                             // specify number of bytes
+		.addr = address,
+		.flags = I2C_FLAG_READ,
+		.buf[1].data = rx_buffer,
+		.buf[1].len = length,
 	};
 	ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write transaction with sensor
 	while(ret == i2cTransferInProgress) {          // continue until all data has been sent
@@ -154,19 +110,15 @@ void i2c_read(uint8_t address,uint8_t* rx_buffer, int length)
 }
 
 
-void i2c_write_read(uint8_t address,uint8_t* tx_buffer,int lengthtx,uint8_t* rx_buffer,int lengthrx)
+void i2c_write_read(uint8_t address, uint8_t* tx_buffer,int lengthtx,uint8_t* rx_buffer,int lengthrx)
 {
-	//uint8_t tx_buffer[17]= {0x03,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0xDF,0xBE};           // software tx buffer
-	//uint8_t rx_buffer[7];           // software rx buffer
-	I2C_TransferReturn_TypeDef ret; // I2C state tracker
-	/* Send power-up command to sensor (see TSL2569 datasheet for details) */
-	//tx_buffer[] = {0xAC,0x02,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0x35,0xC0};
+	I2C_TransferReturn_TypeDef ret;
 	I2C_TransferSeq_TypeDef sensor_message =
 	{
-	    .addr = address,//SENSOR_SLAVE_ADDR,//(SENSOR_SLAVE_ADDR << 1),            // set sensor slave address
-	    .flags = I2C_FLAG_WRITE_READ,//I2C_FLAG_WRITE,                     // indicate basic write
-	    .buf[0].data = tx_buffer,                    // point to tx_buffer
-	    .buf[0].len = lengthtx,//15,                             // specify number of bytes
+	    .addr = address,
+	    .flags = I2C_FLAG_WRITE_READ,
+	    .buf[0].data = tx_buffer,
+	    .buf[0].len = lengthtx,
 		.buf[1].data = rx_buffer,
 		.buf[1].len = lengthrx,
 	};
@@ -177,95 +129,3 @@ void i2c_write_read(uint8_t address,uint8_t* tx_buffer,int lengthtx,uint8_t* rx_
 	}
 }
 
-
-
-
-
-void read_sensor()
-{
-	  uint16_t i;
-	  uint8_t tx_buffer[17]= {0x03,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0xDF,0xBE};           // software tx buffer
-	  uint8_t rx_buffer[7];           // software rx buffer
-	  I2C_TransferReturn_TypeDef ret; // I2C state tracker
-	/* Send power-up command to sensor (see TSL2569 datasheet for details) */
-	  //tx_buffer[] = {0xAC,0x02,0x00,0xA4,0x00,0x07,0xD2,0x76,0x00,0x00,0x85,0x01,0x01,0x00,0x35,0xC0};
-	  I2C_TransferSeq_TypeDef sensor_message =
-	  	  {
-	  	    .addr = SENSOR_SLAVE_ADDR,//(SENSOR_SLAVE_ADDR << 1),            // set sensor slave address
-	  	    .flags = I2C_FLAG_WRITE,//I2C_FLAG_WRITE,                     // indicate basic write
-	  	    .buf[0].data = tx_buffer,                    // point to tx_buffer
-	  	    .buf[0].len = 15,                             // specify number of bytes
-	  	  };
-
-	  ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write transaction with sensor
-	  	  while(ret == i2cTransferInProgress) {          // continue until all data has been sent
-	  	    ret = I2C_Transfer(I2C0);
-	  	  }
-
-	  tx_buffer[0] = 0x02;//0xD0;                           // acces register
-	  tx_buffer[1] = 0x90;//0x3;                            //
-	  tx_buffer[2] = 0x00;//0x3;                            // offset = 8
-	  tx_buffer[3] = 0xF1;//0x3;
-	  tx_buffer[4] = 0x09;//0x3;
-
-	  sensor_message.flags = I2C_FLAG_READ;//I2C_FLAG_WRITE,                     // indicate basic write
-	  sensor_message.buf[0].data = tx_buffer;                    // point to tx_buffer
-	  sensor_message.buf[0].len = 5;                             // specify number of bytes
-	  sensor_message.buf[1].len = 5;                 // specify # bytes to be read
-	  sensor_message.buf[1].data = rx_buffer;
-
-	  ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write transaction with sensor
-	  while(ret == i2cTransferInProgress) {          // continue until all data has been sent
-	    ret = I2C_Transfer(I2C0);
-	  }
-
-	  /* Read part# and rev ID from sensor (see TSL2569 datasheet for details) */
-	  tx_buffer[0] = 0x00;//0xD0;                           // acces register
-	  tx_buffer[1] = 0x0B;//0x3;                            //
-	  tx_buffer[2] = 0x00;//0x3;                            // offset = 8
-	  tx_buffer[3] = 0x08;//0x3;                            // offset = 8
-	  tx_buffer[4] = 0x07;
-
-	  sensor_message.flags = I2C_FLAG_WRITE;         //I2C_FLAG_READ;//I2C_FLAG_WRITE_READ;    // indicate combined write/read
-	  sensor_message.buf[0].len = 5;                 // specify # bytes to be written
-	  sensor_message.buf[0].data = tx_buffer;        // point to tx_buffer
-	  sensor_message.buf[1].len = 7;                 // specify # bytes to be read
-	  sensor_message.buf[1].data = rx_buffer;        // point to rx_buffer
-
-	  ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write/read transaction with sensor
-	  while(ret == i2cTransferInProgress) {          // continue until all data has been received
-	    ret = I2C_Transfer(I2C0);
-	  }
-
-	  sensor_message.flags = I2C_FLAG_READ;
-	  ret = I2C_TransferInit(I2C0, &sensor_message); // start I2C write/read transaction with sensor
-	  	  while(ret == i2cTransferInProgress) {          // continue until all data has been received
-	  	    ret = I2C_Transfer(I2C0);
-	  	  }
-
-	  print_byte_setup(rx_buffer[0]);                // break result up into individual characters
-
-	  /* Print Part# RevID */
-	  for(i=0; i<sizeof(id_string); i++) {
-	    //LEUART_Tx(LEUART0, id_string[i]);
-	  }
-	  for(i=0; i<sizeof(print_byte_array); i++) {
-	    //LEUART_Tx(LEUART0, print_byte_array[i]);
-	  }
-}
-// This function breaks up 'byte' into high and low nibbles (ASCII characters) to be transmitted over UART
-void print_byte_setup(uint8_t byte) {
-
-  if (((byte & 0xF0) >> 4) <= 0x09) {                      // if high nibble is less than 0xA
-    print_byte_array[2] = ((byte & 0xF0) >> 4) + 0x30;     // store ASCII char
-  }
-  if ((((byte & 0xF0) >> 4) >= 0x0A) && (((byte & 0xF0) >> 4)<= 0x0F)) { // if high nibble is between 0xA and 0xF
-    print_byte_array[2] = ((byte & 0xF0) >> 4) + 0x37;     // store ASCII char
-  }
-  if ((byte & 0x0F) <= 0x09) {                             // if low nibble is less than 0xA
-    print_byte_array[3] = (byte & 0x0F) + 0x30;            // store ASCII char
-  }
-  if (((byte & 0x0F) >= 0x0A) && ((byte & 0x0F)<= 0x0F)) { // if low nibble is between 0xA and 0xF
-    print_byte_array[3] = (byte & 0x0F) + 0x37;            // store ASCII char
-  }
-}
--- stack/framework/hal/inc/hwi2c.h
+++ stack/framework/hal/inc/hwi2c.h
@@ -1,32 +1,59 @@
-/*
- * hwi2c.h
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
  *
- *  Created on: Aug 25, 2015
- *      Author: peryton
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+/*! \file hwi2c.h
+ * \addtogroup I2C
+ * \ingroup HAL
+ * @{
+ * \brief Provides an API for I2C
+ * \author maarten.weyn@uantwerpen.be
+ * \author jens.vanhooydonck@student.uantwerpen.be
+ */
+
 
 #ifndef I2C_H_
 #define I2C_H_
 
 #include "types.h"
 #include "link_c.h"
+#include "em_gpio.h"
+#include "em_i2c.h"
 
 
-typedef struct {
-  uint8_t com_code;
-  uint8_t byte_cnt;
-  uint8_t data[];
-}i2c_message_typeDef;
 
-__LINK_C void i2c_init();
-__LINK_C void i2c_write(uint8_t address,uint8_t* tx_buffer,int length);
-__LINK_C void i2c_read(uint8_t address,uint8_t* rx_buffer, int length);
-__LINK_C void i2c_write_read(uint8_t address,uint8_t* tx_buffer,int lengthtx,uint8_t* rx_buffer,int lengthrx);
+typedef struct
+{
+  I2C_TypeDef           *port;          /**< Peripheral port */
+  GPIO_Port_TypeDef     sclPort;        /**< SCL pin port number */
+  uint8_t               sclPin;         /**< SCL pin number */
+  GPIO_Port_TypeDef     sdaPort;        /**< SDA pin port number */
+  uint8_t               sdaPin;         /**< SDA pin number */
+  uint8_t               portLocation;   /**< Port location */
+  uint32_t              i2cRefFreq;     /**< I2C reference clock */
+  uint32_t              i2cMaxFreq;     /**< I2C max bus frequency to use */
+  I2C_ClockHLR_TypeDef  i2cClhr;        /**< Clock low/high ratio control */
+} I2C_Init_T;
 
-/* I2C functions */
-__LINK_C void print_byte_setup(uint8_t byte);
 
-
+__LINK_C void i2c_master_init(I2C_Init_T* init);
+__LINK_C void i2c_write(uint8_t address, uint8_t* tx_buffer, int length);
+__LINK_C void i2c_read(uint8_t address, uint8_t* rx_buffer, int length);
+__LINK_C void i2c_write_read(uint8_t address, uint8_t* tx_buffer, int lengthtx, uint8_t* rx_buffer, int lengthrx);
 
 
 #endif /* FRAMEWORK_HAL_INC_HWI2C_H_ */
