45f6b9cf18270d3af0ba90eb7a1f16bb30c03f23
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2015-10-28 11:38:26 +0100

    implement watchdog API for EFM32HG

---
+++ stack/framework/hal/chips/efm32hg/efm32hg_watchdog.c
@@ -0,0 +1,38 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file efm32hg_watchdog.c
+ *
+ *  \author glenn.ergeerts@uantwerpen.be
+ *
+ */
+
+#include "hwwatchdog.h"
+#include "em_wdog.h"
+#include <assert.h>
+
+void __watchdog_init()
+{
+    WDOG_Init_TypeDef wdog_init = WDOG_INIT_DEFAULT;
+    WDOG_Init(&wdog_init);
+}
+
+void hw_watchdog_feed()
+{
+    WDOG_Feed();
+}
--- stack/framework/hal/platforms/EFM32HG_STK3400/stk3400_main.c
+++ stack/framework/hal/platforms/EFM32HG_STK3400/stk3400_main.c
@@ -56,6 +56,8 @@ void __platform_init()
     error_t err;
     err = hw_gpio_configure_pin(BUTTON0, true, gpioModeInput, 0); assert(err == SUCCESS); // TODO pull up or pull down to prevent floating
     err = hw_gpio_configure_pin(BUTTON1, true, gpioModeInput, 0); assert(err == SUCCESS); // TODO pull up or pull down to prevent floating
+
+    __watchdog_init(); // TODO configure from cmake?
 }
 
 void __platform_post_framework_init()
