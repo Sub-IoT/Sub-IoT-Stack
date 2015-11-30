dbf9b2d6487d592bd656e17a9136af2bb31ac33e
Maarten Weyn <maarten.weyn@uantwerpen.be>
2015-11-30 15:23:48 +0100

    Revert "Merge remote-tracking branch 'origin/uart-redesign' into ERZ32WG"
    This reverts commit 69dd7cb03961c3b71e88ffb0c9e6de77833f2ee4, reversing
    changes made to c5c18551bc9492ef503ad047634d3773c4d5e7c5.

--- stack/framework/hal/chips/cc1101/cc1101_interface_spi.c
+++ stack/framework/hal/chips/cc1101/cc1101_interface_spi.c
@@ -55,12 +55,7 @@ void _cc1101_interface_init(end_of_packet_isr_t end_of_packet_isr_cb)
 {
     end_of_packet_isr_callback = end_of_packet_isr_cb;
 
-    spi_init((spi_definition_t){
-      .usart     = CC1101_SPI_USART,
-      .baudrate  = CC1101_SPI_BAUDRATE,
-      .databits  = 8,
-      .location  = CC1101_SPI_LOCATION
-    });
+    spi_init();
 
     error_t err;
     err = hw_gpio_configure_interrupt(CC1101_GDO0_PIN, &_cc1101_gdo_isr, GPIO_FALLING_EDGE); assert(err == SUCCESS);
@@ -84,20 +79,20 @@ void _c1101_interface_set_interrupts_enabled(bool enable)
 
 uint8_t _c1101_interface_strobe(uint8_t strobe)
 {
-    spi_select(CC1101_SPI_PIN_CS);
-    uint8_t statusByte = spi_byte(CC1101_SPI_USART, strobe & 0x3F);
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_select_chip();
+    uint8_t statusByte = spi_byte(strobe & 0x3F);
+    spi_deselect_chip();
 
     return statusByte;
 }
 
 uint8_t _c1101_interface_reset_radio_core()
 {
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_deselect_chip();
     hw_busy_wait(30);
-    spi_select(CC1101_SPI_PIN_CS);
+    spi_select_chip();
     hw_busy_wait(30);
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_deselect_chip();
     hw_busy_wait(45);
 
     cc1101_interface_strobe(RF_SRES);          // Reset the Radio Core
@@ -107,10 +102,10 @@ uint8_t _c1101_interface_reset_radio_core()
 static uint8_t readreg(uint8_t addr)
 {
 
-    spi_select(CC1101_SPI_PIN_CS);
-    spi_byte(CC1101_SPI_USART, (addr & 0x3F) | READ_SINGLE);
-    uint8_t val = spi_byte(CC1101_SPI_USART, 0); // send dummy byte to receive reply
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_select_chip();
+    spi_byte((addr & 0x3F) | READ_SINGLE);
+    uint8_t val = spi_byte(0); // send dummy byte to receive reply
+    spi_deselect_chip();
 
     DPRINT("READ REG 0x%02X @0x%02X", val, addr);
 
@@ -121,13 +116,13 @@ static uint8_t readstatus(uint8_t addr)
 {
     uint8_t ret, retCheck, data, data2;
     uint8_t _addr = (addr & 0x3F) | READ_BURST;
-    spi_select(CC1101_SPI_PIN_CS);
-    ret = spi_byte(CC1101_SPI_USART, _addr);
-    data = spi_byte(CC1101_SPI_USART, 0); // send dummy byte to receive reply
+    spi_select_chip();
+    ret = spi_byte(_addr);
+    data = spi_byte(0); // send dummy byte to receive reply
     // See CC1101's Errata for SPI read errors // TODO needed?
 //    while (true) {
-//    	retCheck = spi_byte(CC1101_SPI_USART, _addr);
-//        data2 = spi_byte(CC1101_SPI_USART, 0);
+//    	retCheck = spi_byte(_addr);
+//        data2 = spi_byte(0);
 //    	if (ret == retCheck && data == data2)
 //    		break;
 //    	else {
@@ -136,7 +131,7 @@ static uint8_t readstatus(uint8_t addr)
 //    	}
 //    }
 
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_deselect_chip();
 
     DPRINT("READ STATUS 0x%02X @0x%02X", data, addr);
 
@@ -154,28 +149,28 @@ uint8_t _c1101_interface_read_single_reg(uint8_t addr)
 
 void _c1101_interface_write_single_reg(uint8_t addr, uint8_t value)
 {
-    spi_select(CC1101_SPI_PIN_CS);
-    spi_byte(CC1101_SPI_USART, (addr & 0x3F));
-    spi_byte(CC1101_SPI_USART, value);
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_select_chip();
+    spi_byte((addr & 0x3F));
+    spi_byte(value);
+    spi_deselect_chip();
 }
 
 void _c1101_interface_read_burst_reg(uint8_t addr, uint8_t* buffer, uint8_t count)
 {
     uint8_t _addr = (addr & 0x3F) | READ_BURST;
-    spi_select(CC1101_SPI_PIN_CS);
-    spi_byte(CC1101_SPI_USART, _addr);
-    spi_string(CC1101_SPI_USART,  NULL, buffer, count );
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_select_chip();
+    spi_byte(_addr);
+    spi_string( NULL, buffer, count );
+    spi_deselect_chip();
 }
 
 void _c1101_interface_write_burst_reg(uint8_t addr, uint8_t* buffer, uint8_t count)
 {
     uint8_t _addr = (addr & 0x3F) | WRITE_BURST;
-    spi_select(CC1101_SPI_PIN_CS);
-    spi_byte(CC1101_SPI_USART, _addr);
-    spi_string(CC1101_SPI_USART,  buffer, NULL, count );
-    spi_deselect(CC1101_SPI_PIN_CS);
+    spi_select_chip();
+    spi_byte(_addr);
+    spi_string( buffer, NULL, count );
+    spi_deselect_chip();
 }
 
 void _c1101_interface_write_single_patable(uint8_t value)
--- stack/framework/hal/platforms/EFM32GG_STK3700/stk3700_main.c
+++ stack/framework/hal/platforms/EFM32GG_STK3700/stk3700_main.c
@@ -40,7 +40,7 @@ void __platform_init()
     __efm32gg_mcu_init();
     __gpio_init();
     __uart_init();
-    // __led_init();    // uses ports assigned to USART1
+    __led_init();
     __lcd_init();
 
 #ifdef PLATFORM_USE_USB_CDC
