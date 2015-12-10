5f13270647827d4339df841755ae03de94ea4d66
Maarten Weyn <maarten.weyn@uantwerpen.be>
2015-12-10 16:50:17 +0100

    port to EZ32LG-si4460

---
+++ stack/framework/hal/chips/si4460/si4460_interface.h
@@ -0,0 +1,68 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *
+ *  Authors:
+ * 		maarten.weyn@uantwerpen.be
+ */
+
+#ifndef SI4455_INTERFACE_H
+#define SI4455_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ecode.h"
+
+#include "hwradio.h"
+
+// Error code definitions of EZRadioDrv
+#define ECODE_EMDRV_EZRADIODRV_OK                       ( ECODE_OK )                                   ///< Success return value.
+#define ECODE_EMDRV_EZRADIODRV_ILLEGAL_HANDLE           ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000001 )   ///< Illegal SPI handle.
+
+#define ECODE_EMDRV_EZRADIODRV_TRANSMIT_PLUGIN_BASE             ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000100 )   ///< Transmit plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_RECEIVE_PLUGIN_BASE              ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000200 )   ///< Receive plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_CRC_ERROR_PLUGIN_BASE            ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000300 )   ///< CRC error plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_AUTO_ACK_PLUGIN_BASE             ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000400 )   ///< Receive plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_UNMOD_CARRIER_PLUGIN_BASE        ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000500 )   ///< Receive plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_PN9_PLUGIN_BASE                  ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000600 )   ///< Receive plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_DIRECT_TRANSMIT_PLUGIN_BASE      ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000700 )   ///< Transmit plugin base error code.
+#define ECODE_EMDRV_EZRADIODRV_DIRECT_RECEIVE_PLUGIN_BASE       ( ECODE_EMDRV_EZRADIODRV_BASE | 0x00000800 )   ///< Receive plugin base error code.
+
+// Transmit plugin related error codes
+#define ECODE_EMDRV_EZRADIODRV_TRANSMIT_FAILED          		( ECODE_EMDRV_EZRADIODRV_TRANSMIT_PLUGIN_BASE | 0x00000001 )   ///< Unable to start transmission.
+
+
+typedef void (*ezradio_int_callback_t)();
+
+void ezradioInit(ezradio_int_callback_t cb);
+void ezradioResetTRxFifo(void);
+Ecode_t ezradioStartRx(uint8_t channel);
+Ecode_t ezradioStartTx(hw_radio_packet_t* packet, uint8_t channel_id, bool rx_after);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SI4455_INTERFACE_H */
---
+++ stack/framework/hal/platforms/EZR32LG_WSTK6200A/inc/platform.h
@@ -0,0 +1,120 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __PLATFORM_H_
+#define __PLATFORM_H_
+
+#include "platform_defs.h"
+
+#ifndef PLATFORM_EZR32LG_WSTK6200A
+    #error Mismatch between the configured platform and the actual platform. Expected PLATFORM_EZR32LG_WSTK6200A to be defined
+#endif
+
+
+#include "ezr32wg_chip.h"
+
+/********************
+ * LED DEFINITIONS *
+ *******************/
+
+#define HW_NUM_LEDS 2
+#define LED0	F6
+#define	LED1	F7
+
+/********************
+ *  USB SUPPORT      *
+ ********************/
+
+#define USB_DEVICE
+
+/********************
+ * UART DEFINITIONS *
+ *******************/
+
+#define UART_ENABLED
+
+#define UART_BAUDRATE PLATFORM_EZR32LG_WSTK6200A_UART_BAUDRATE
+
+
+
+#define UART_CHANNEL        UART0
+#define UART_CLOCK          cmuClock_UART0
+#define UART_ROUTE_LOCATION UART_ROUTE_LOCATION_LOC1
+
+// UART0 location #1: PE0 and PE1
+#define UART_PIN_TX         E0           // PE0
+#define UART_PIN_RX         E1          // PE1
+
+
+/********************
+ * SPI RF DEFINITIONS *
+ *******************/
+
+/* SPI Channel configuration */
+//#define SPI_CHANNEL         USART0                      // SPI Channel
+//#define SPI_BAUDRATE        6000000                    // SPI Frequency
+//#define SPI_CLOCK           cmuClock_USART0             // SPI Clock
+//#define SPI_ROUTE_LOCATION  USART_ROUTE_LOCATION_LOC0   // SPI GPIO Routing
+
+/* SPI Ports and Pins for the selected route location above.
+ * See the datasheet for the availiable routes and corresponding GPIOs */
+//#define SPI_PIN_MOSI        E10
+//#define SPI_PIN_MISO        E11
+//#define SPI_PIN_CLK         E12
+//#define SPI_PIN_CS          E9
+
+// si4455 PIN definitions
+//#ifdef USE_si4455
+    #define si4455_GDO0_PIN A15
+    #define si4455_GDO1_PIN E14
+    #define si4455_SDN_PIN  E8
+//#endif
+
+//#ifdef SPI_USE_DMA
+//
+//    /* DMA Channel configuration */
+//    #define DMA_CHANNEL_TX      0
+//    #define DMA_CHANNEL_RX      1
+//    #define DMA_CHANNELS        2
+//    #define DMA_REQ_RX          DMAREQ_USART1_RXDATAV
+//    #define DMA_REQ_TX          DMAREQ_USART1_TXBL
+//#endif
+
+/*************************
+ * DEBUG PIN DEFINITIONS *
+ ************************/
+
+#define DEBUG_PIN_NUM 4
+#define DEBUG0	D6
+#define DEBUG1	D3
+#define DEBUG2	D4
+#define DEBUG3	D5
+
+/**************************
+ * USERBUTTON DEFINITIONS *
+ *************************/
+
+#define NUM_USERBUTTONS 	2
+#define BUTTON0				E3
+#define BUTTON1				E2
+
+
+
+#define HAS_LCD
+
+#endif
