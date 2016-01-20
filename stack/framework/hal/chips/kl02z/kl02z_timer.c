8fe4430c11a00dafd654e6929e369cf9ab5e07ed
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-01-20 16:26:08 +0100

    kl02z working, but with shortcuts

--- stack/framework/hal/chips/kl02z/kl02z_timer.c
+++ stack/framework/hal/chips/kl02z/kl02z_timer.c
@@ -31,77 +31,12 @@
 #include "MKL02Z4.h"
 #include "fsl_tpm_hal.h"
 
-/**************************************************************************//**
- * @brief  Start LFRCO for RTC
- * Starts the low frequency RC oscillator (LFRCO) and routes it to the RTC
- *****************************************************************************/
-//void startLfxoForRtc(uint8_t freq)
-//{
-//    /* Starting LFRCO and waiting until it is stable */
-//    CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
-//
-//    /* Routing the LFRCO clock to the RTC */
-//    CMU_ClockSelectSet(cmuClock_LFA,cmuSelect_LFXO);
-//    CMU_ClockEnable(cmuClock_RTC, true);
-//
-//    /* Set Clock prescaler */
-//    if(freq == HWTIMER_FREQ_1MS)
-//    	CMU_ClockDivSet(cmuClock_RTC, cmuClkDiv_32);
-//    else
-//    	CMU_ClockDivSet(cmuClock_RTC, cmuClkDiv_1);
-//
-//    /* Enabling clock to the interface of the low energy modules */
-//    CMU_ClockEnable(cmuClock_CORELE, true);
-//}
-
 static timer_callback_t compare_f = 0x0;
 static timer_callback_t overflow_f = 0x0;
 static bool timer_inited = false;
-/**************************************************************************//**
- * @brief Enables LFACLK and selects LFXO as clock source for RTC.
- *        Sets up the RTC to count at 1024 Hz.
- *        The counter should not be cleared on a compare match and keep running.
- *        Interrupts should be cleared and enabled.
- *        The counter should run.
- *****************************************************************************/
+
 error_t hw_timer_init(hwtimer_id_t timer_id, uint8_t frequency, timer_callback_t compare_callback, timer_callback_t overflow_callback)
 {
-// TODO
-//    if(timer_id >= HWTIMER_NUM)
-//    	return ESIZE;
-//    if(timer_inited)
-//    	return EALREADY;
-//    if(frequency != HWTIMER_FREQ_1MS && frequency != HWTIMER_FREQ_32K)
-//    	return EINVAL;
-//    start_atomic();
-//		compare_f = compare_callback;
-//		overflow_f = overflow_callback;
-//		timer_inited = true;
-//
-//		/* Configuring clocks in the Clock Management Unit (CMU) */
-//		startLfxoForRtc(frequency);
-//
-//		RTC_Init_TypeDef rtcInit = RTC_INIT_DEFAULT;
-//		rtcInit.enable   = false;   /* Don't enable RTC after init has run */
-//		rtcInit.comp0Top = true;   /* Clear counter on compare 0 match: cmp 0 is used to limit the value of the rtc to 0xffff */
-//		rtcInit.debugRun = false;   /* Counter shall not keep running during debug halt. */
-//
-//
-//		/* Initialize the RTC */
-//		RTC_Init(&rtcInit);
-//
-//		//disable all rtc interrupts while we're still configuring
-//		RTC_IntDisable(RTC_IEN_OF | RTC_IEN_COMP0 | RTC_IEN_COMP1);
-//		RTC_IntClear(RTC_IFC_OF | RTC_IFC_COMP0 | RTC_IFC_COMP1);
-//		//Set maximum value for the RTC
-//		RTC_CompareSet( 0, 0x0000FFFF );
-//		RTC_CounterReset();
-//
-//		RTC_IntEnable(RTC_IEN_COMP0);
-//
-//		NVIC_EnableIRQ(RTC_IRQn);
-//		RTC_Enable(true);
-//    end_atomic();
 
     if(timer_id >= HWTIMER_NUM) return ESIZE;
     if(timer_inited) return EALREADY;
@@ -110,15 +45,15 @@ error_t hw_timer_init(hwtimer_id_t timer_id, uint8_t frequency, timer_callback_t
     overflow_f = overflow_callback;
     compare_f = compare_callback;
 
-    SIM_SOPT2 = (SIM_SOPT2&~SIM_SOPT2_TPMSRC_MASK)|SIM_SOPT2_TPMSRC(3);//select MCGIRCLK to reference (4MHz) // TODO use 1 kHz LPO input clock instead
-    SIM_SCGC6 |= (1<<SIM_SCGC6_TPM0_SHIFT);//enable bus clock to module
-    TPM0_CNT = 0x00;//clear count register
+    SIM_SOPT2 = (SIM_SOPT2&~SIM_SOPT2_TPMSRC_MASK)|SIM_SOPT2_TPMSRC(3); //select MCGIRCLK to reference (4MHz) // TODO use 1 kHz LPO input clock instead + validate accuracy
+    SIM_SCGC6 |= (1<<SIM_SCGC6_TPM0_SHIFT); //enable bus clock to module
+    TPM0_CNT = 0x00;
     TPM0_MOD = 0xFFFF;
     TPM0_SC = (1<<TPM_SC_TOF_SHIFT)|(0<<TPM_SC_TOIE_SHIFT)|(0<<TPM_SC_CPWMS_SHIFT)|(TPM_SC_CMOD(1)|TPM_SC_PS(5));//TOF cleared, interrupt disable, clock references "module clock,"
                                                                                                                  //prescale 4 MHz to 125000 Hz, timer counts up
     TPM_HAL_SetCpwms(TPM0, 1);
     TPM_HAL_EnableChnInt(TPM0, 0);
-    MCG_C1 |= (1<<MCG_C1_IRCLKEN_SHIFT);//start reference clock
+    MCG_C1 |= (1<<MCG_C1_IRCLKEN_SHIFT); //start reference clock
 
     NVIC_ClearPendingIRQ(TPM0_IRQn);
     NVIC_DisableIRQ(TPM0_IRQn);
@@ -141,7 +76,8 @@ error_t hw_timer_schedule(hwtimer_id_t timer_id, hwtimer_tick_t tick )
     if(!timer_inited) return EOFF;
 
     TPM0_CNT = 0x00;    //clear count register
-    TPM_HAL_SetChnCountVal(TPM0, 0, 125 /** tick*/); // TODO validate + test overflow
+    TPM0_MOD = 125 * tick; // TODO validate accuracy + change when switching ref clock
+    //TPM_HAL_SetChnCountVal(TPM0, 0, 125 /** tick*/); // TODO use compare instead of MOD
     NVIC_EnableIRQ(TPM0_IRQn);
     TPM_HAL_EnableTimerOverflowInt(TPM0);
 }
@@ -161,66 +97,43 @@ error_t hw_timer_counter_reset(hwtimer_id_t timer_id)
     if(!timer_inited) return EOFF;
 
     NVIC_DisableIRQ(TPM0_IRQn);
-    TPM0_CNT = 0x00;    //clear count register
+    TPM0_CNT = 0x00;
     NVIC_EnableIRQ(TPM0_IRQn);
 }
 
 bool hw_timer_is_overflow_pending(hwtimer_id_t timer_id)
 {
 	// TODO
-//    if(timer_id >= HWTIMER_NUM)
-//	return false;
-//    start_atomic();
-//	//COMP0 is used to limit thc RTC to 16 bits -> use this one to check
-//	bool is_pending = !!((RTC_IntGet() & RTC->IEN) & RTC_IFS_COMP0);
-//    end_atomic();
-//    return is_pending;
 }
 bool hw_timer_is_interrupt_pending(hwtimer_id_t timer_id)
 {
-// TODO
-//    if(timer_id >= HWTIMER_NUM)
-//	return false;
-//
-//    start_atomic();
-//	bool is_pending = !!((RTC_IntGet() & RTC->IEN) & RTC_IFS_COMP1);
-//    end_atomic();
-//    return is_pending;
+    // TODO
 }
 
 void TPM0_IRQHandler()
 {
+// TODO we are using MOD register for configured interval causing overflow interrupt when timer elapses.
+// When this happens call compare_f callback. This means we currently do not support real timer overflow (for long intervals),
+// until we find out how to implement compare functionality with TPM.
+
+//    NVIC_DisableIRQ(TPM0_IRQn);
+//    if(TPM_HAL_GetTimerOverflowStatus(TPM0) && overflow_f != 0)
+//    {
+//        TPM_HAL_ClearTimerOverflowFlag(TPM0);
+//        overflow_f();
+//    }
+//    else if(compare_f != 0)
+//    {
+//        TPM_HAL_ClearChnInt(TPM0, 0);
+//        compare_f();
+//    }
+
     NVIC_DisableIRQ(TPM0_IRQn);
-    if(TPM_HAL_GetTimerOverflowStatus(TPM0) && overflow_f != 0)
-    {
-        TPM_HAL_ClearTimerOverflowFlag(TPM0);
-        overflow_f();
-    }
-    else if(compare_f != 0)
-    {
-        TPM_HAL_ClearChnInt(TPM0, 0);
+    TPM_HAL_DisableTimerOverflowInt(TPM0);
+    TPM_HAL_ClearTimerOverflowFlag(TPM0);
+    TPM_HAL_ClearChnInt(TPM0, 0);
+    if(compare_f != 0)
         compare_f();
-    }
 
 }
 
-
-// TODO
-//INT_HANDLER(RTC_IRQHandler)
-//{
-//	//retrieve flags. We 'OR' this with the enabled interrupts
-//	//since the COMP1 flag may be set if it wasn't used before (compare register == 0 -> ifs flag set regardless of whether interrupt is enabled)
-//	//by AND ing with the IEN we make sure we only consider the flags of the ENABLED interrupts
-//	uint32_t flags = (RTC_IntGet() & RTC->IEN);
-//	RTC_IntClear(RTC_IFC_OF | RTC_IFC_COMP0 | RTC_IFC_COMP1);
-//
-//	//evaluate flags to see which one(s) fired:
-//	if((flags & RTC_IFS_COMP0) && (overflow_f != 0x0))
-//		overflow_f();
-//	if((flags & RTC_IFS_COMP1))
-//	{
-//		RTC_IntDisable(RTC_IEN_COMP1);
-//		if(compare_f != 0x0)
-//			compare_f();
-//	}
-//}
