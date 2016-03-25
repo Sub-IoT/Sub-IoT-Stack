792ca6ea1364b82d739ea734265636b11e284ad0
Jens Vanhooydonck <jensvanhooydonck@gmail.com>
2016-03-25 11:38:23 +0100

    Update platform.h

--- stack/framework/hal/platforms/EFM32HG_STK3400/inc/platform.h
+++ stack/framework/hal/platforms/EFM32HG_STK3400/inc/platform.h
@@ -54,6 +54,7 @@
 /********************
  * UART DEFINITIONS *
  *******************/
+ 
 // console configuration
 #define CONSOLE_UART        PLATFORM_EFM32HG_STK3400_CONSOLE_UART
 #define CONSOLE_LOCATION    PLATFORM_EFM32HG_STK3400_CONSOLE_LOCATION
