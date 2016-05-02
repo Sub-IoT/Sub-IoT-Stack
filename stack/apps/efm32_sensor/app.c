8d45b3590a62c5eb9b56f01f2dc12deb70f232c6
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-05-02 09:50:27 +0200

    use sensible transmittion timeout

--- stack/apps/efm32_sensor/app.c
+++ stack/apps/efm32_sensor/app.c
@@ -174,7 +174,7 @@ void bootstrap()
             .control_number_of_subbands = 1,
             .subnet = 0x00,
             .scan_automation_period = 0,
-            .transmission_timeout_period = 0xFF, // TODO compressed time value
+            .transmission_timeout_period = 0x10,
             .subbands[0] = (subband_t){
                 .channel_header = {
                     .ch_coding = PHY_CODING_PN9,
