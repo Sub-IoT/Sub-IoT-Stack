aa87c7d1018cd18f767b4e96a1179e8198fc33a5
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-05-03 13:49:25 +0200

    use 20 ticks for transmission timeout

--- stack/apps/gateway/app.c
+++ stack/apps/gateway/app.c
@@ -55,7 +55,7 @@ void bootstrap()
             .control_number_of_subbands = 1,
             .subnet = 0,
             .scan_automation_period = 0,
-            .transmission_timeout_period = 0xFF,
+            .transmission_timeout_period = 20,
             .subbands[0] = (subband_t){
                 .channel_header = {
                     .ch_coding = PHY_CODING_PN9,
