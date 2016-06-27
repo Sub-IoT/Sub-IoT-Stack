a925a58a5e61b944add1d60101cbaa50ea68b447
smartdsp <jung.hoon.lee@cortus.com>
2016-06-27 17:00:00 +0900

    Modified comment of license

--- stack/framework/hal/chips/cortus/Bsp/machine/sfradr.h
+++ stack/framework/hal/chips/cortus/Bsp/machine/sfradr.h
@@ -1,19 +1,31 @@
-/*********************************************************************************
- * This confidential and proprietary software may be used only as authorized 
- *                      by a licensing agreement from                           
- *                           Cortus S.A.
- *
- *             (C) Copyright 2004, 2005, 2006 Cortus S.A.
- *                           ALL RIGHTS RESERVED
- *
- * The entire notice above must be reproduced on all authorized copies
- * and any such reproduction must be pursuant to a licensing agreement 
- * from Cortus S.A. (http://www.cortus.com)
- *
- * $CortusRelease$
- * $FileName$
- *
- *********************************************************************************/
+/*******************************************************************************
+* File: sfradr.h
+* @section License
+* <b>(C) Copyright 2005 Cortus S.A, http://www.cortus.com
+*******************************************************************************
+*
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not
+*    claim that you wrote the original software.
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*
+* DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Cortus S.A has no
+* obligation to support this Software. Cortus S.A is providing the
+* Software "AS IS", with no express or implied warranties of any kind,
+* including, but not limited to, any implied warranties of merchantability
+* or fitness for any particular purpose or warranties against infringement
+* of any proprietary rights of a third party.
+*
+* Cortus S.A will not be liable for any consequential, incidental, or
+* special damages, or any other relief, or for any claim by any third party,
+* arising from your use of this Software.
+*
+******************************************************************************/
 
 #ifndef _SFRADR_H
 #define _SFRADR_H
