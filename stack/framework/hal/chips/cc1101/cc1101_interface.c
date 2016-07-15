d60a0b5d818366d8d3d0ab0f9d2e2e70b1bce3fd
Philippe Nunes <philippe.nunes@cortus.com>
2016-07-15 11:22:14 +0200

    Fix the physical layer logging configuration
    Signed-off-by: Philippe Nunes <philippe.nunes@cortus.com>

--- stack/framework/hal/chips/cc1101/cc1101_interface.c
+++ stack/framework/hal/chips/cc1101/cc1101_interface.c
@@ -30,7 +30,7 @@
 #include "debug.h"
 
 // turn on/off the debug prints
-#if defined(FRAMEWORK_LOG_ENABLED) && defined(FRAMEWORK_LOG_PHY_ENABLED)
+#if defined(FRAMEWORK_LOG_ENABLED) && defined(FRAMEWORK_PHY_LOG_ENABLED)
     #define DPRINT(...) log_print_stack_string(LOG_STACK_PHY, __VA_ARGS__)
 #else
     #define DPRINT(...)
