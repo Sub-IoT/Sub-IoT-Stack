559c7657233f4d8b294e131d14fff2d6f6fcc33e
Philippe Nunes <philippe.nunes@cortus.com>
2016-09-19 12:14:17 +0200

    Integrate Tiny-AES128-C as the foundation of our AES library.
    Tiny-AES128-C is a public implementation of the AES128 ECB and CBC
    encryption algorithms written in C.
    
    The code has slightly been cleaned up for consistency with OSS7 coding rules

---
+++ stack/framework/inc/aes.h
@@ -0,0 +1,41 @@
+#ifndef _AES_H_
+#define _AES_H_
+
+#include <stdint.h>
+
+
+// #define the macros below to 1/0 to enable/disable the mode of operation.
+//
+// CBC enables AES128 encryption in CBC-mode of operation and handles 0-padding.
+// ECB enables the basic ECB 16-byte block algorithm. Both can be enabled simultaneously.
+
+// The #ifndef-guard allows it to be configured before #include'ing or at compile time.
+#ifndef CBC
+  #define CBC 0 // We don't need this mode in the latest DASH7 specification
+#endif
+
+#ifndef ECB
+  #define ECB 1
+#endif
+
+
+
+#if defined(ECB) && ECB
+
+// The two functions AES128_ECB_xxcrypt() do most of the work, and they expect inputs of 128 bit length.
+void AES128_ECB_encrypt(uint8_t *input, const uint8_t *key, uint8_t *output);
+void AES128_ECB_decrypt(uint8_t *input, const uint8_t *key, uint8_t *output);
+
+#endif // #if defined(ECB) && ECB
+
+
+#if defined(CBC) && CBC
+
+void AES128_CBC_encrypt_buffer(uint8_t *output, uint8_t *input, uint32_t length, const uint8_t *key, const uint8_t *iv);
+void AES128_CBC_decrypt_buffer(uint8_t *output, uint8_t *input, uint32_t length, const uint8_t *key, const uint8_t *iv);
+
+#endif // #if defined(CBC) && CBC
+
+
+
+#endif //_AES_H_
