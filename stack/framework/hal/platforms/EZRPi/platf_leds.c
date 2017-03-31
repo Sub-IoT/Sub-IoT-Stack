dc200519d0f636b843d11a37e5fd2baa6d518f2b
Christophe VG <contact@christophe.vg>
2017-03-31 14:08:35 +0200

    contributed first EZRPi platform implementation

---
+++ stack/framework/hal/platforms/EZRPi/platf_leds.c
@@ -0,0 +1,71 @@
+/* OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2017 Christophe VG <contact@christophe.vg>
+ * Copyright 2017 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file platf_leds.c
+ *
+ *  \author contact@christophe.vg
+ *
+ */
+
+#include <debug.h>
+
+#include "em_gpio.h"
+
+#include "hwleds.h"
+
+#include "timer.h"
+
+#include "platform.h"
+
+#include "led.h"
+
+#if HW_NUM_LEDS != 1
+	#error HW_NUM_LEDS does not match the expected value. Update platform.h or platf_leds.c
+#endif
+
+void __led_init() {
+	error_t err = hw_gpio_configure_pin(LED0, false, gpioModePushPull, 0);
+	assert(err == SUCCESS);
+}
+
+void led_on(uint8_t led_nr) {
+	hw_gpio_set(LED0);
+}
+
+void led_off(unsigned char led_nr) {
+	hw_gpio_clr(LED0);
+}
+
+void led_toggle(unsigned char led_nr) {
+  hw_gpio_toggle(LED0);
+}
+
+// flashing support
+
+static void end_flash()  { led_off(LED_BLUE);  }
+
+void led_flash() {
+  led_on(LED_BLUE);
+  timer_post_task_delay(&end_flash, FLASH_DURATION);
+}
+
+bool led_init() {
+  sched_register_task(&end_flash);
+  return true;
+}
---
+++ stack/framework/hal/platforms/EZRPi/platf_main.c
@@ -0,0 +1,60 @@
+/* OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2017 Christophe VG <contact@christophe.vg>
+ * Copyright 2017 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file platf_main.c
+ *
+ *  \author contact@christophe.vg
+ *
+ */
+
+#include "ezr32lg_mcu.h"
+
+#include "hwgpio.h"
+#include "hwleds.h"
+
+#include "scheduler.h"
+#include "bootstrap.h"
+
+#include "platform.h"
+
+#include "led.h"
+
+void __platform_init() {
+	__ezr32lg_mcu_init();
+  __gpio_init();
+  __led_init();
+}
+
+void __platform_post_framework_init() {
+  led_init();
+}
+
+int main(){
+  // initialise the platform itself
+	__platform_init();
+
+  // do not initialise the scheduler, this is done by __framework_bootstrap()
+  __framework_bootstrap();
+
+  // initialise platform functionality that depends on the framework
+  __platform_post_framework_init();
+
+  scheduler_run();
+  return 0;
+}
