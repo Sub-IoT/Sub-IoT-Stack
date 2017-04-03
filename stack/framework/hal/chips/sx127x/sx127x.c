f20ca79654846bb81907a1e2f2f245e8c2909756
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-04-03 14:00:57 +0200

    working on sx127x driver: - writing registers and fifo works - add initial register settings for sending on 868N000 - hw_radio_send_packet() is working

--- stack/framework/hal/chips/sx127x/sx127x.c
+++ stack/framework/hal/chips/sx127x/sx127x.c
@@ -35,6 +35,7 @@
 
 #include "sx1276Regs-Fsk.h"
 
+#define FREQ_STEP 61.03515625
 
 #if defined(FRAMEWORK_LOG_ENABLED) && defined(FRAMEWORK_PHY_LOG_ENABLED)
 #define DPRINT(...) log_print_stack_string(LOG_STACK_PHY, __VA_ARGS__)
@@ -58,11 +59,18 @@
 #endif
 
 typedef enum {
-    HW_RADIO_STATE_IDLE,
-    HW_RADIO_STATE_TX,
-    HW_RADIO_STATE_RX,
-    HW_RADIO_STATE_OFF,
-    HW_RADIO_STATE_UNKOWN
+  OPMODE_SLEEP = 0,
+  OPMODE_STANDBY = 1,
+  OPMODE_FSTX = 2,
+  OPMODE_TX = 3,
+  OPMODE_FSRX = 4,
+  OPMODE_RX = 5,
+} opmode_t;
+
+typedef enum {
+  STATE_IDLE,
+  STATE_TX,
+  STATE_RX
 } state_t;
 
 static spi_slave_handle_t* sx127x_spi = NULL;
@@ -70,9 +78,9 @@ static alloc_packet_callback_t alloc_packet_callback;
 static release_packet_callback_t release_packet_callback;
 static rx_packet_callback_t rx_packet_callback;
 static tx_packet_callback_t tx_packet_callback;
+static state_t state = STATE_IDLE;
+static hw_radio_packet_t* current_packet;
 //static rssi_valid_callback_t rssi_valid_callback;
-//static state_t current_state;
-//static hw_radio_packet_t* current_packet;
 //static channel_id_t current_channel_id = {
 //    .channel_header_raw = 0xFF,
 //    .center_freq_index = 0xFF
@@ -84,6 +92,137 @@ static uint8_t read_reg(uint8_t addr) {
   uint8_t value = spi_exchange_byte(sx127x_spi, 0x00); // get the response
   spi_deselect(sx127x_spi);
   return value;
+}
+
+static void write_reg(uint8_t addr, uint8_t value) {
+  spi_select(sx127x_spi);
+  spi_exchange_byte(sx127x_spi, addr | 0x80); // send address with bit 8 high to signal a write operation
+  spi_exchange_byte(sx127x_spi, value);
+  spi_deselect(sx127x_spi);
+}
+
+static void write_fifo(uint8_t* buffer, uint8_t size) {
+  spi_select(sx127x_spi);
+  spi_exchange_byte(sx127x_spi, 0x00 | 0x80); // send address with bit 8 high to signal a write operation
+  spi_exchange_bytes(sx127x_spi, buffer, NULL, size);
+  spi_deselect(sx127x_spi);
+}
+
+static void set_opmode(opmode_t opmode) {
+  write_reg(REG_OPMODE, (read_reg(REG_OPMODE) & RF_OPMODE_MASK) | opmode);
+}
+
+static void init_regs() {
+  write_reg(REG_OPMODE, 0x00); // FSK, hi freq, sleep
+
+  // modulation settings, defaulting to normal channel class
+  // BR 0x0240 => 55555.55555 bps
+  write_reg(REG_BITRATEMSB, 0x02);
+  write_reg(REG_BITRATELSB, 0x40);
+
+  // Fdev => 49.988 kHz
+  write_reg(REG_FDEVMSB, 0x03);
+  write_reg(REG_FDEVLSB, 0x33);
+
+  // centr freq, default to 868N000: 863.1 MHz
+  uint32_t freq = 863100000; // in Hz
+  freq = (uint32_t)((double)freq / (double)FREQ_STEP);
+  write_reg(REG_FRFMSB, (uint8_t)((freq >> 16) & 0xFF));
+  write_reg(REG_FRFMID, (uint8_t)((freq >> 8) & 0xFF));
+  write_reg(REG_FRFLSB, (uint8_t)(freq & 0xFF));
+
+  // PA
+  write_reg(REG_PACONFIG, 0x7F); // no PA BOOST & use max output power
+  write_reg(REG_PARAMP, (2 << 5) & 0x09); // BT=0.5 and PaRamp=40us // TODO
+
+  //  write_reg(REG_OCP, 0); // TODO default for now
+  write_reg(REG_LNA, 0x23); // highest gain for now, for 868
+  //  write_reg(REG_RXCONFIG, 0); // TODO default for now
+  //  write_reg(REG_RSSICONFIG, 0); // TODO default for now
+  //  write_reg(REG_RSSICOLLISION, 0); // TODO default for now
+  //  write_reg(REG_RSSITHRESH, 0); // TODO default for now
+
+  // RX
+  // channel bandwidth 203.125 kHz, data rate 55.542 kBaud
+  // Carson's rule: 2 x fm + 2 x fd  = 55.555 + 2 x 50 = 155.555 kHz
+  // assuming 10 ppm crystals gives max error of: 2 * 10 ppm * 433.16 = 8.66 kHz
+  // => BW > 155.555 + 8.66 kHz => > 164 kHZ. Closest possible value is 166.7
+  write_reg(REG_RXBW, (2 << 3) & 1);
+  //  write_reg(REG_AFCBW, 0); // TODO default for now
+  //  write_reg(REG_AFCFEI, 0); // TODO default for now
+  //  write_reg(REG_AFCMSB, 0); // TODO default for now
+  //  write_reg(REG_AFCLSB, 0); // TODO default for now
+//  write_reg(REG_PREAMBLEDETECT, 0); // TODO
+//  write_reg(REG_RXTIMEOUT1, 0); // TODO
+//  write_reg(REG_RXTIMEOUT1, 0); // TODO
+//  write_reg(REG_RXTIMEOUT1, 0); // TODO
+//  write_reg(REG_RXDELAY, 0); // TODO
+//  write_reg(REG_OSC, 0x07); // keep as default: off
+  write_reg(REG_PREAMBLEMSB, 0x00);
+  write_reg(REG_PREAMBLELSB, 0x04); // TODO 6 for hi rate
+  write_reg(REG_SYNCCONFIG, 0); // no AutoRestartRx, default PreambePolarity, enable syncword of 2 bytes
+  write_reg(REG_SYNCVALUE1, 0x0B);
+  write_reg(REG_SYNCVALUE2, 0x67);
+  write_reg(REG_PACKETCONFIG1, 0xC8); // var length, whitening, addressFiltering off. TODO CRC
+  write_reg(REG_PACKETCONFIG2, 0x40); // packet mode
+//  write_reg(REG_PAYLOADLENGTH, 0); // TODO
+//  write_reg(REG_FIFOTHRESH, 0); // TODO
+//  write_reg(REG_SEQCONFIG1, 0); // TODO
+//  write_reg(REG_SEQCONFIG2, 0); // TODO
+//  write_reg(REG_TIMERRESOL, 0); // TODO
+//  write_reg(REG_TIMER1COEF, 0); // TODO
+//  write_reg(REG_TIMER2COEF, 0); // TODO
+//  write_reg(REG_IMAGECAL, 0); // TODO
+//  write_reg(REG_LOWBAT, 0); // TODO
+  write_reg(REG_DIOMAPPING1, 0); // TODO
+  write_reg(REG_DIOMAPPING2, 0x30); // ModeReady TODO
+//  write_reg(REG_PLLHOP, 0); // TODO
+//  write_reg(REG_TCXO, 0); // TODO
+//  write_reg(REG_PADAC, 0); // TODO
+//  write_reg(REG_BITRATEFRAC, 0); // TODO
+//  write_reg(REG_AGCREF, 0); // TODO
+//  write_reg(REG_AGCTHRESH1, 0); // TODO
+//  write_reg(REG_AGCTHRESH2, 0); // TODO
+//  write_reg(REG_AGCTHRESH3, 0); // TODO
+//  write_reg(REG_PLL, 0); // TODO
+
+
+  // TODO validate:
+  // bitrate
+  // GFSK settings
+  // preamble detector
+  // packet handler
+  // sync word
+  // packet len
+  // CRC
+  // whitening
+  // PA
+
+  // TODO burst write reg?
+}
+
+static void end_of_packet_isr() {
+  log_print_string("end of packet ISR");
+  if(state = STATE_TX) {
+    set_opmode(OPMODE_STANDBY);
+    if(tx_packet_callback) {
+      current_packet->tx_meta.timestamp = timer_get_counter_value();
+      tx_packet_callback(current_packet);
+    }
+  } else {
+    assert(false); // TODO RX
+  }
+}
+
+static void dio_isr(pin_id_t pin_id, uint8_t event_mask) {
+  log_print_string("DIO IRQ\n");
+  if(hw_gpio_pin_matches(pin_id, SX127x_DIO0_PIN)) {
+    hw_gpio_disable_interrupt(SX127x_DIO0_PIN);
+    end_of_packet_isr();
+  }
+  else
+     assert(false); // other DIO pins not used (yet)
+
 }
 
 error_t hw_radio_init(alloc_packet_callback_t alloc_packet_cb, release_packet_callback_t release_packet_cb) {
@@ -98,6 +237,15 @@ error_t hw_radio_init(alloc_packet_callback_t alloc_packet_cb, release_packet_ca
 
   spi_handle_t* spi_handle = spi_init(SX127x_SPI_USART, SX127x_SPI_BAUDRATE, 8, true, SX127x_SPI_LOCATION);
   sx127x_spi = spi_init_slave(spi_handle, SX127x_SPI_PIN_CS, true);
+
+  init_regs();
+  set_opmode(OPMODE_STANDBY); // TODO sleep
+  // TODO reset ?
+  // TODO op mode
+
+  error_t e = hw_gpio_configure_interrupt(SX127x_DIO0_PIN, &dio_isr, GPIO_RISING_EDGE); assert(e == SUCCESS);
+
+  // TODO def regs
 
   return SUCCESS; // TODO FAIL return code
 }
@@ -119,7 +267,15 @@ bool hw_radio_is_rx() {
 }
 
 error_t hw_radio_send_packet(hw_radio_packet_t* packet, tx_packet_callback_t tx_callback) {
-  // TODO
+
+  tx_packet_callback = tx_callback;
+  current_packet = packet;
+  hw_gpio_enable_interrupt(SX127x_DIO0_PIN);
+  log_print_string("TX len=%i", packet->length);
+  write_reg(REG_PAYLOADLENGTH, packet->length);
+  write_fifo(packet->data, packet->length);
+  state = STATE_TX;
+  set_opmode(OPMODE_TX);
 }
 
 error_t hw_radio_send_background_packet(hw_radio_packet_t* packet, tx_packet_callback_t tx_callback, uint16_t eta, uint16_t tx_duration) {
@@ -132,4 +288,13 @@ error_t hw_radio_start_background_scan(hw_rx_cfg_t const* rx_cfg, rx_packet_call
 
 int16_t hw_radio_get_rssi() {
   // TODO
+}
+
+void hw_radio_continuous_tx(hw_tx_cfg_t const* tx_cfg, bool continuous_wave) {
+  // TODO tx_cfg
+  assert(!continuous_wave); // TODO CW not supported for now
+  write_reg(REG_PAYLOADLENGTH, 0);
+  write_fifo(NULL, 50); // TODO
+  set_opmode(OPMODE_TX);
+  log_print_string("cont tx\n");
 }
