530d83a3ca41b021cb7464a89f168e0a10b0456d
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-06-01 12:13:07 +0200

    configure CC1101 GDO2 PIN on STK3700

--- stack/framework/hal/platforms/EFM32GG_STK3700/platf_main.c
+++ stack/framework/hal/platforms/EFM32GG_STK3700/platf_main.c
@@ -48,6 +48,7 @@ void __platform_init()
     // configure the interrupt pins here, since hw_gpio_configure_pin() is MCU
     // specific and not part of the common HAL API
     hw_gpio_configure_pin(CC1101_GDO0_PIN, true, gpioModeInput, 0);
+    hw_gpio_configure_pin(CC1101_GDO2_PIN, true, gpioModeInput, 0);
     // hw_gpio_configure_pin(CC1101_SPI_PIN_CS, false, gpioModePushPull, 1);
 #endif
 #ifdef USE_SX127X
