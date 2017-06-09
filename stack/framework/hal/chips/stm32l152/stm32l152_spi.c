de3dd63c2faa8042391089ba84b524d45e7f28ea
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-06-09 17:26:55 +0200

    STM32L SPI: small refactorings + start the pheriperal clock on init

--- stack/framework/hal/chips/stm32l152/stm32l152_spi.c
+++ stack/framework/hal/chips/stm32l152/stm32l152_spi.c
@@ -31,14 +31,10 @@
 #include "stm32l152_pins.h"
 #include "stm32l1xx_hal.h"
 
- #define USARTS    4
+#define SPI_COUNT 3
 #define MAX_SPI_SLAVE_HANDLES 4        // TODO expose this in chip configuration
 
  typedef struct {
-//   pin_id_t mosi;
-//   pin_id_t miso;
-//   pin_id_t clk;
-	 uint8_t id;
    uint32_t pins;
    uint32_t alternate;
    GPIO_TypeDef* port;
@@ -53,54 +49,27 @@
  };
 
  // TODO to be completed with all documented locations
- static spi_pins_t usarts[USARTS] = {
+ static spi_pins_t spis[SPI_COUNT] = {
    {
-     // USART 0
-
-//       .mosi    = { .port = 0xFF, .pin = 0xFF },
-//       .miso    = { .port = 0xFF, .pin = 0xFF },
-//       .clk     = { .port = 0xFF, .pin = 0xFF },
-	   .port 	= 0,
-	   .pins 	= 0,
-	   .alternate = 0,
-	   .spi = 0
-
+     // SPI1
+     .port 	= GPIOA,
+     .pins 	= GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, // A5=SCK, A6=MISO, A7=MOSI
+     .alternate = GPIO_AF5_SPI1,
+     .spi = SPI1
    },
    {
-        // USART 1
-
-//          .mosi     = { .port = 0, .pin = 7 },
-//          .miso     = { .port = 0, .pin = 6 },
-//          .clk      = { .port = 0, .pin = 5 },
-		  .port 	= GPIOA,
-		  .pins 	= GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7,
-		  .alternate = GPIO_AF5_SPI1,
-		  .spi = SPI1
-
-      },
-   {
-     // USART 2
-
-//       .mosi     = { .port = 1, .pin = 15 },
-//       .miso     = { .port = 1, .pin = 14 },
-//       .clk      = { .port = 1, .pin = 13 },
+      // SPI2
 	   .port 	= GPIOB,
 	   .pins 	 = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15,
 	   .alternate = GPIO_AF5_SPI2,
-		  .spi = SPI2
-
+     .spi = SPI2
    },
    {
-     // USART 3
-
-//       .mosi     = { .port = 2, .pin = 12 },
-//       .miso     = { .port = 2, .pin = 11 },
-//       .clk      = { .port = 2, .pin = 10 },
+     // SPI3
 	   .port 	= GPIOC,
 	   .pins 	 = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12,
 	   .alternate = GPIO_AF6_SPI3,
-		  .spi = SPI3
-
+     .spi = SPI3
    }
  };
 
@@ -114,17 +83,14 @@
    uint8_t             slaves;  // number of slaves for array mgmt
    uint8_t             users;   // for reference counting of active slaves
    bool                active;
-   uint8_t		id;
  };
 
  // private storage for handles, pointers to these records are passed around
- static spi_handle_t handle[USARTS] =
- 	 	 	 {
- 	 	 			 {.id=0, .hspi.Instance=NULL},
-					 {.id=1, .hspi.Instance=NULL},
-					 {.id=2, .hspi.Instance=NULL},
-					 {.id=3, .hspi.Instance=NULL}
- 	 	 	 };
+ static spi_handle_t handle[SPI_COUNT] = {
+   {.hspi.Instance=NULL},
+   {.hspi.Instance=NULL},
+   {.hspi.Instance=NULL},
+};
 
  static void ensure_slaves_deselected(spi_handle_t* spi) {
    // make sure CS lines for all slaves of this bus are high for active low
@@ -145,22 +111,24 @@
    // bringing SPI bus up
    ensure_slaves_deselected(spi);
 
-   switch (spi->id)
+   switch ((uint32_t)(spi->hspi.Instance))
       {
-      case 1:
+      case SPI1_BASE:
    	   __HAL_RCC_SPI1_CLK_ENABLE();
    	   break;
-      case 2:
+      case SPI2_BASE:
    	   __HAL_RCC_SPI2_CLK_ENABLE();
    	   break;
-      case 3:
+      case SPI3_BASE:
    	   __HAL_RCC_SPI3_CLK_ENABLE();
    	   break;
+     default:
+       assert(false);
       }
 
   if (HAL_SPI_Init(&(spi->hspi)) != HAL_OK)
 	{
-	assert ("cannot init");
+  assert(false);
 	return;
 	}
 
@@ -173,15 +141,15 @@
 
    HAL_SPI_DeInit(&spi->hspi);
 
-   switch (spi->id)
+   switch ((uint32_t)(spi->hspi.Instance))
 	{
-	case 1:
+  case SPI1_BASE:
 	   __HAL_RCC_SPI1_CLK_ENABLE();
 	   break;
-	case 2:
+  case SPI2_BASE:
 	   __HAL_RCC_SPI2_CLK_ENABLE();
 	   break;
-	case 3:
+  case SPI3_BASE:
 	   __HAL_RCC_SPI3_CLK_ENABLE();
 	   break;
 	}
@@ -195,76 +163,76 @@
    spi->active = false;
  }
   
- spi_handle_t* spi_init(uint8_t uart, uint32_t baudrate, uint8_t databits, bool msbf, uint8_t pins)
+ spi_handle_t* spi_init(uint8_t spi_number, uint32_t baudrate, uint8_t databits, bool msbf, uint8_t pins)
  {
    // assert what is supported by HW
    assert(databits == 8);
-   assert(pins == 0);
-   assert(uart < USARTS);
-   assert(uart > 0);
+   assert(pins == 0); // TODO alternate location?
+   assert(spi_number < SPI_COUNT);
 
-   if (handle[uart].hspi.Instance != NULL)
+   if (handle[spi_number].hspi.Instance != NULL)
    {
 	   //TODO: check if settings are ok
-	   return &handle[uart];
+     return &handle[spi_number];
    }
 
    GPIO_InitTypeDef GPIO_InitStruct;
-   GPIO_InitStruct.Pin = usarts[uart].pins;
+   GPIO_InitStruct.Pin = spis[spi_number].pins;
    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
    GPIO_InitStruct.Pull = GPIO_NOPULL;
    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-   GPIO_InitStruct.Alternate = usarts[uart].alternate;
-   HAL_GPIO_Init(usarts[uart].port, &GPIO_InitStruct);
+   GPIO_InitStruct.Alternate = spis[spi_number].alternate;
+   HAL_GPIO_Init(spis[spi_number].port, &GPIO_InitStruct);
 
-   handle[uart].hspi.Instance = usarts[uart].spi;
-  	handle[uart].hspi.Init.Mode = SPI_MODE_MASTER;
-  	handle[uart].hspi.Init.Direction = SPI_DIRECTION_2LINES;
-  	handle[uart].hspi.Init.DataSize = SPI_DATASIZE_8BIT;
-  	handle[uart].hspi.Init.CLKPolarity = SPI_POLARITY_LOW;
-  	handle[uart].hspi.Init.CLKPhase = SPI_PHASE_1EDGE;
-  	handle[uart].hspi.Init.NSS = SPI_NSS_SOFT;
+   handle[spi_number].hspi.Instance = spis[spi_number].spi;
+    handle[spi_number].hspi.Init.Mode = SPI_MODE_MASTER;
+    handle[spi_number].hspi.Init.Direction = SPI_DIRECTION_2LINES;
+    handle[spi_number].hspi.Init.DataSize = SPI_DATASIZE_8BIT;
+    handle[spi_number].hspi.Init.CLKPolarity = SPI_POLARITY_LOW;
+    handle[spi_number].hspi.Init.CLKPhase = SPI_PHASE_1EDGE;
+    handle[spi_number].hspi.Init.NSS = SPI_NSS_SOFT;
+
   	switch (baudrate)
   	{
   	case 16000000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
   		break;
   	case 8000000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
   		break;
   	case 4000000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
   		break;
   	case 2000000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
   		break;
   	case 1000000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
   		break;
   	case 500000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
   		break;
   	case 250000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
   		break;
   	case 125000:
-  		handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+      handle[spi_number].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
   		break;
   	default:
   		assert(false);
   	}
 
-  	handle[uart].hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
   	if (msbf)
-  	handle[uart].hspi.Init.FirstBit = SPI_FIRSTBIT_MSB;
+      handle[spi_number].hspi.Init.FirstBit = SPI_FIRSTBIT_MSB;
   	else
-  	handle[uart].hspi.Init.FirstBit = SPI_FIRSTBIT_LSB;
-  	handle[uart].hspi.Init.TIMode = SPI_TIMODE_DISABLE;
-  	handle[uart].hspi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  	handle[uart].hspi.Init.CRCPolynomial = 10;
+      handle[spi_number].hspi.Init.FirstBit = SPI_FIRSTBIT_LSB;
 
-  
-   return &handle[uart];
+    handle[spi_number].hspi.Init.TIMode = SPI_TIMODE_DISABLE;
+    handle[spi_number].hspi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    handle[spi_number].hspi.Init.CRCPolynomial = 10;
+
+    spi_enable(&handle[spi_number]);
+   return &handle[spi_number];
  }
 
  spi_slave_handle_t*  spi_init_slave(spi_handle_t* spi, pin_id_t cs_pin, bool cs_is_active_low) {
@@ -275,7 +243,7 @@
 	GPIO_InitTypeDef GPIO_InitStruct;
 	GPIO_InitStruct.Pin = (1<<cs_pin.pin);
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	error_t err = hw_gpio_configure_pin_stm(cs_pin, &GPIO_InitStruct);
 	assert(err == SUCCESS || err == EALREADY);
@@ -338,7 +306,6 @@
 
  void spi_exchange_bytes(spi_slave_handle_t* slave, uint8_t* TxData, uint8_t* RxData, unsigned int length)
  {
-   uint16_t i = 0;
    if( RxData != NULL && TxData != NULL ) {           // two way transmission
 	   HAL_SPI_TransmitReceive(&slave->spi->hspi, TxData, RxData, length, HAL_MAX_DELAY);
    } else if( RxData == NULL && TxData != NULL ) {    // send only
