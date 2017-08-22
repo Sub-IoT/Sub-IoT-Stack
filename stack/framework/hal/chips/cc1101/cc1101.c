132c00703d430f3d76b9b2c82d461d46b3dbe9fa
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-08-22 16:52:37 +0200

    cc1101: added stubs for background frames API

--- stack/framework/hal/chips/cc1101/cc1101.c
+++ stack/framework/hal/chips/cc1101/cc1101.c
@@ -776,6 +776,18 @@ error_t hw_radio_start_background_scan(hw_rx_cfg_t const* rx_cfg, rx_packet_call
     return SUCCESS;
 }
 
+error_t hw_radio_start_background_advertising(tx_packet_callback_t tx_callback)
+{
+  assert(false);
+  // TODO implement this
+}
+
+error_t hw_radio_set_background(hw_radio_packet_t* packet, uint16_t eta, uint16_t tx_duration)
+{
+  assert(false);
+  // TODO implement this
+}
+
 error_t hw_radio_send_packet(hw_radio_packet_t* packet, tx_packet_callback_t tx_cb)
 {
     // TODO error handling EINVAL, ESIZE, EOFF
