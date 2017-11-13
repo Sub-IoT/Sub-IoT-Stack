369a60524d06da76e3f54cd69d5c473313e745fc
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-11-13 15:43:59 +0100

    lorawan: more cleanups

--- stack/modules/lorawan/ST/stm32l0xx_hw.c
+++ stack/modules/lorawan/ST/stm32l0xx_hw.c
@@ -70,44 +70,6 @@ Maintainer: Miguel Luis and Gregory Cristian
 #define         ID2                                 ( 0x1FF80054 )
 #define         ID3                                 ( 0x1FF80064 )
 
-/*!
- * \brief ADC Vbat measurement constants
- */
-
- /* Internal voltage reference, parameter VREFINT_CAL*/
-#define VREFINT_CAL       ((uint16_t*) ((uint32_t) 0x1FF80078))
-#define LORAWAN_MAX_BAT   254
-
-
-/* Internal temperature sensor: constants data used for indicative values in  */
-/* this example. Refer to device datasheet for min/typ/max values.            */
-
-/* Internal temperature sensor, parameter TS_CAL1: TS ADC raw data acquired at 
- *a temperature of 110 DegC (+-5 DegC), VDDA = 3.3 V (+-10 mV). */
-#define TEMP30_CAL_ADDR   ((uint16_t*) ((uint32_t) 0x1FF8007A))
-
-/* Internal temperature sensor, parameter TS_CAL2: TS ADC raw data acquired at 
- *a temperature of  30 DegC (+-5 DegC), VDDA = 3.3 V (+-10 mV). */
-#define TEMP110_CAL_ADDR  ((uint16_t*) ((uint32_t) 0x1FF8007E))
-
-/* Vdda value with which temperature sensor has been calibrated in production 
-   (+-10 mV). */
-#define VDDA_TEMP_CAL                  ((uint32_t) 3000)        
-
-
-#define COMPUTE_TEMPERATURE(TS_ADC_DATA, VDDA_APPLI)                           \
-  ((((( ((int32_t)((TS_ADC_DATA * VDDA_APPLI) / VDDA_TEMP_CAL)                  \
-        - (int32_t) *TEMP30_CAL_ADDR)                                          \
-     ) * (int32_t)(110 - 30)                                                   \
-    )<<8) / (int32_t)(*TEMP110_CAL_ADDR - *TEMP30_CAL_ADDR)                        \
-   ) + (30<<8)                                                                      \
-  )
-
-static ADC_HandleTypeDef hadc;
-/*!
- * Flag to indicate if the ADC is Initialized
- */
-static bool AdcInitialized = false;
 
 /*!
  * Flag to indicate if the MCU is Initialized
@@ -134,10 +96,6 @@ void HW_Init( void )
     
     HW_SPI_Init( );
 
-    //vcom_Init( );
-    
-    //BSP_sensor_Init( );
-
     McuInitialized = true;
   }
 }
@@ -152,22 +110,8 @@ void HW_DeInit( void )
   HW_SPI_DeInit( );
   
   Radio.IoDeInit( );
-  
-  vcom_DeInit( );
-   
-  McuInitialized = false;
-}
 
-/**
-  * @brief This function Initializes the hardware Ios
-  * @param None
-  * @retval None
-  */
-static void HW_IoInit( void )
-{
-  Radio.IoInit( );
-  
-  vcom_IoInit( );
+  McuInitialized = false;
 }
 
 /**
@@ -178,71 +122,8 @@ static void HW_IoInit( void )
 static void HW_IoDeInit( void )
 { 
   Radio.IoDeInit( );
-  
-  vcom_IoDeInit( );
 }
 
-
-void HW_GpioInit(void)
-{
-  /* STM32L0 Gpios are all already configured in analog input at nReset*/
-}
-
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (HSI)
-  *            SYSCLK(Hz)                     = 32000000
-  *            HCLK(Hz)                       = 32000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 1
-  *            APB2 Prescaler                 = 1
-  *            HSI Frequency(Hz)              = 16000000
-  *            PLLMUL                         = 6
-  *            PLLDIV                         = 3
-  *            Flash Latency(WS)              = 1
-  * @retval None
-  */
-
-void SystemClock_Config( void )
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-
-  /* Enable HSE Oscillator and Activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSEState            = RCC_HSE_OFF;
-  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLLMUL_6;
-  RCC_OscInitStruct.PLL.PLLDIV          = RCC_PLLDIV_3;
-
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    assert(false);
-  }
-
-  /* Set Voltage scale1 as MCU will run at 32MHz */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  
-  /* Poll VOSF bit of in PWR_CSR. Wait until it is reset to 0 */
-  while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET) {};
-
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-  clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    assert(false);
-  }
-}
 /**
   * @brief This function return a random seed
   * @note based on the device unique ID
@@ -327,7 +208,7 @@ void HW_ExitStopMode( void)
   while( __HAL_RCC_GET_SYSCLK_SOURCE( ) != RCC_SYSCLKSOURCE_STATUS_PLLCLK ) {}
     
   /*initilizes the peripherals*/
-  HW_IoInit( );
+  //HW_IoInit( );
 
   RESTORE_PRIMASK( );
 }
