00d9e052e9932ae48d4abb8126cf0913f38e7027
Philippe Nunes <philippe.nunes@cortus.com>
2017-12-15 16:07:30 +0100

    Cortus build: override _assert_func to abort the CPU
    To comply with the assert.h as implemented by the aps-mini lib,
    the __assert_func is declared in debug.h if __ASSERT_FUNC is not
    defined.

---
+++ stack/framework/hal/platforms/cortus_fpga/libc_overrides.c
@@ -0,0 +1,41 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hwuart.h"
+#include "hwatomic.h"
+#include "hwleds.h"
+#include "log.h"
+
+//we override __assert_func to abort the CPU and get the stack trace
+void __assert_func( const char *file, int line, const char *func, const char *failedexpr)
+{
+    start_atomic();
+    led_on(0);
+    led_on(1);
+    abort();
+}
+
+size_t strnlen(const char *str, size_t maxlen)
+{
+  const char *start = str;
+
+  while (maxlen-- > 0 && *str)
+    str++;
+
+  return str - start;
+}
