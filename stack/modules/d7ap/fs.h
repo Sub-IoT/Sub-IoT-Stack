fa2e313216efe9170253f2d699932c6e32802477
Philippe Nunes <philippe.nunes@cortus.com>
2018-01-09 16:09:04 +0100

    Fix Cortus Build

--- stack/modules/d7ap/fs.h
+++ stack/modules/d7ap/fs.h
@@ -68,15 +68,15 @@ typedef enum
     FS_STORAGE_PERMANENT = 3
 } fs_storage_class_t;
 
-typedef struct
+typedef struct __attribute__((__packed__))
 {
     fs_storage_class_t storage_class : 2;
     uint8_t _rfu : 2;
     alp_act_condition_t action_condition : 3;
     bool action_protocol_enabled : 1;
-} __packed fs_file_properties_t;
+} fs_file_properties_t;
 
-typedef struct
+typedef struct __attribute__((__packed__))
 {
     uint8_t file_permissions; // TODO not used for now
     fs_file_properties_t file_properties;
@@ -84,7 +84,7 @@ typedef struct
     uint8_t interface_file_id;
     uint32_t length;
     uint32_t allocated_length;
-} __packed fs_file_header_t;
+} fs_file_header_t;
 
 /**
  * \brief Initialize the user files in this callback.
