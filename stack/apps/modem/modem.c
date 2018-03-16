7a3874fd31c10c52dd67a9ca28e32613734621fd
Maarten Weyn <maarten.weyn@uantwerpen.be>
2018-03-16 17:01:31 +0100

    modem print uid

--- stack/apps/modem/modem.c
+++ stack/apps/modem/modem.c
@@ -39,6 +39,9 @@
 void bootstrap()
 {
     log_print_string("Device booted\n");
+    uint8_t uid[8];
+    fs_read_uid(uid);
+    log_print_string("UID %02X%02X%02X%02X%02X%02X%02X%02X", uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6], uid[7]);
 
     fs_init_args_t fs_init_args = (fs_init_args_t){
         .access_profiles_count = DEFAULT_ACCESS_PROFILES_COUNT,
