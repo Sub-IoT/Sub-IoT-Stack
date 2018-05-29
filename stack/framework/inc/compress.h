325570a54e15c67d6f0914600b068f39a892d8fd
Maarten Weyn <maarten@wesdec.be>
2018-05-29 15:51:41 +0200

    added compressed time typdef

--- stack/framework/inc/compress.h
+++ stack/framework/inc/compress.h
@@ -38,6 +38,14 @@
 #include <stdbool.h>
 #include "math.h"
 
+typedef union{
+  uint8_t raw;
+  struct {
+    uint8_t exp : 3; // Exponent (ranging from 0 to 7)
+    uint8_t mant : 5; // Mantissa (randing from 0 to 31)
+  };
+} compressed_time_t;
+
 #define CT_DECOMPRESS(ct) (pow(4, ct >> 5) * (ct & 0b11111))
 
 uint8_t compress_data(uint16_t value, bool ceil);
