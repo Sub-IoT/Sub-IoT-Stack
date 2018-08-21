55c1d76f11203d19fe44abd9dd821ae8cb56a3e6
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2018-08-21 11:37:28 +0200

    hw_radio_io_(de)init is now also implemented for nucleo-l073 platform

--- stack/framework/hal/platforms/NUCLEO_L073RZ/platf_main.c
+++ stack/framework/hal/platforms/NUCLEO_L073RZ/platf_main.c
@@ -30,13 +30,73 @@
 #include "stm32_common_gpio.h"
 
 #if defined(USE_SX127X) && defined(PLATFORM_SX127X_USE_RESET_PIN)
-static void reset_sx127x()
+// override the weak definition
+// TODO might be moved to radio driver is hw_gpio_configure_pin() is part of public HAL API instead of platform specific
+void hw_radio_reset()
 {
   error_t e;
   e = hw_gpio_configure_pin(SX127x_RESET_PIN, false, GPIO_MODE_OUTPUT_PP, 0); assert(e == SUCCESS);
   hw_busy_wait(150);
   e = hw_gpio_configure_pin(SX127x_RESET_PIN, false, GPIO_MODE_INPUT, 1); assert(e == SUCCESS);
   hw_busy_wait(10000);
+}
+#endif
+
+#if defined(USE_SX127X)
+// override the weak definition
+void hw_radio_io_init() {
+  // configure the radio GPIO pins here, since hw_gpio_configure_pin() is MCU
+  // specific and not part of the common HAL API
+  hw_gpio_configure_pin(SX127x_DIO0_PIN, true, GPIO_MODE_INPUT, 0);
+  hw_gpio_disable_interrupt(SX127x_DIO0_PIN);
+  hw_gpio_configure_pin(SX127x_DIO1_PIN, true, GPIO_MODE_INPUT, 0);
+  hw_gpio_disable_interrupt(SX127x_DIO1_PIN);
+#ifdef PLATFORM_SX127X_USE_MANUAL_RXTXSW_PIN
+    hw_gpio_configure_pin(SX127x_MANUAL_RXTXSW_PIN, false, GPIO_MODE_OUTPUT_PP, 0);
+#endif
+  #ifdef PLATFORM_SX127X_USE_DIO3_PIN
+  hw_gpio_configure_pin(SX127x_DIO3_PIN, true, GPIO_MODE_INPUT, 0);
+  hw_gpio_disable_interrupt(SX127x_DIO3_PIN);
+#endif
+#ifdef PLATFORM_SX127X_USE_VCC_TXCO
+  hw_gpio_configure_pin(SX127x_VCC_TXCO, false, GPIO_MODE_OUTPUT_PP, 1);
+  hw_gpio_set(SX127x_VCC_TXCO);
+#endif
+}
+
+// override the weak definition
+void hw_radio_io_deinit() {
+  GPIO_InitTypeDef initStruct={0};
+  initStruct.Mode = GPIO_MODE_ANALOG;
+
+  hw_gpio_configure_pin_stm(SX127x_DIO0_PIN, &initStruct);
+  hw_gpio_configure_pin_stm(SX127x_DIO1_PIN, &initStruct);
+#ifdef PLATFORM_SX127X_USE_DIO3_PIN
+  hw_gpio_configure_pin_stm(SX127x_DIO3_PIN, &initStruct);
+#endif
+#ifdef PLATFORM_SX127X_USE_MANUAL_RXTXSW_PIN
+#endif
+  hw_gpio_configure_pin_stm(SX127x_MANUAL_RXTXSW_PIN, &initStruct);
+#ifdef PLATFORM_SX127X_USE_RESET_PIN
+  hw_gpio_configure_pin_stm(SX127x_RESET_PIN, &initStruct);
+#endif
+#ifdef PLATFORM_SX127X_USE_VCC_TXCO
+  hw_gpio_configure_pin_stm(SX127x_VCC_TXCO, &initStruct);
+#endif
+
+  // TODO do not deinit SPI for now, may be used for other slave as well
+//  hw_gpio_configure_pin_stm(SX127x_SPI_PIN_CS, &initStruct);
+//  hw_gpio_configure_pin_stm(PIN(GPIO_PORTA, 6), &initStruct);
+//  hw_gpio_configure_pin_stm(PIN(GPIO_PORTA, 7), &initStruct);
+//  hw_gpio_configure_pin_stm(PIN(GPIO_PORTB, 3), &initStruct);
+
+  // TODO remove here, ports can be used for other purposes, needs to be managed by gpio driver
+//  __HAL_RCC_GPIOA_CLK_DISABLE();
+//  __HAL_RCC_GPIOB_CLK_DISABLE();
+//  __HAL_RCC_GPIOC_CLK_DISABLE();
+//  __HAL_RCC_GPIOD_CLK_DISABLE();
+//  __HAL_RCC_GPIOE_CLK_DISABLE();
+//  __HAL_RCC_GPIOH_CLK_DISABLE();
 }
 #endif
 
@@ -47,25 +107,11 @@ void __platform_init()
     __hw_debug_init();
 
 #ifdef USE_SX127X
+    hw_radio_io_init();
 
-    // configure the interrupt pins here, since hw_gpio_configure_pin() is MCU
-    // specific and not part of the common HAL API
-    hw_gpio_configure_pin(SX127x_DIO0_PIN, true, GPIO_MODE_INPUT, 0);
-    hw_gpio_configure_pin(SX127x_DIO1_PIN, true, GPIO_MODE_INPUT, 0);
-#ifdef PLATFORM_SX127X_USE_DIO3_PIN
-    hw_gpio_configure_pin(SX127x_DIO3_PIN, true, GPIO_MODE_INPUT, 0);
-#endif
-#ifdef PLATFORM_SX127X_USE_RESET_PIN
-    hw_gpio_configure_pin(SX127x_RESET_PIN, false, GPIO_MODE_OUTPUT_PP, 1);
-    reset_sx127x();
-#endif
-#ifdef PLATFORM_SX127X_USE_MANUAL_RXTXSW_PIN
-    hw_gpio_configure_pin(SX127x_MANUAL_RXTXSW_PIN, false, GPIO_MODE_OUTPUT_PP, 0);
-#endif
-#endif
-
-#if defined(USE_SX127X) && defined(PLATFORM_SX127X_USE_RESET_PIN)
-    reset_sx127x();
+  #ifdef PLATFORM_SX127X_USE_RESET_PIN
+    hw_radio_reset();
+  #endif
 #endif
 
     HAL_EnableDBGSleepMode(); // TODO impact on power?
