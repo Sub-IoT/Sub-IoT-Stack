4f4fe63609ba5c3701d032c51148043107abcf47
niels heirman <niels.heiran@aloxy.io>
2018-09-20 17:18:37 +0200

    Allow dll state to change from IDLE to IDLE

--- stack/modules/d7ap/dll.c
+++ stack/modules/d7ap/dll.c
@@ -213,7 +213,8 @@ static void switch_state(dll_state_t next_state)
                || dll_state == DLL_STATE_CCA1
                || dll_state == DLL_STATE_CCA2
                || dll_state == DLL_STATE_CSMA_CA_RETRY
-               || dll_state == DLL_STATE_SCAN_AUTOMATION);
+               || dll_state == DLL_STATE_SCAN_AUTOMATION
+               || dll_state == DLL_STATE_IDLE);
         dll_state = next_state;
         DPRINT("Switched to DLL_STATE_IDLE");
         break;
