c424adae670b2c92bed70469deb2217418d8a6c7
niels heirman <niels.heiran@aloxy.io>
2019-02-01 11:25:31 +0100

    added erase chip and block erase to hwblcokdevice

--- stack/framework/hal/common/hwblockdevice.c
+++ stack/framework/hal/common/hwblockdevice.c
@@ -17,3 +17,12 @@ error_t blockdevice_program(blockdevice_t* bd, uint8_t* data, uint32_t addr, uin
   return bd->driver->program(bd, data, addr, size);
 }
 
+error_t blockdevice_erase_chip(blockdevice_t* bd){
+  assert(bd && bd->driver && bd->driver->erase_chip);
+  return bd->driver->erase_chip(bd);
+}
+error_t blockdevice_erase_block32k(blockdevice_t* bd, uint32_t addr){
+  assert(bd && bd->driver && bd->driver->erase_block32k);
+  return bd->driver->erase_block32k(bd, addr);
+}
+
--- stack/framework/hal/inc/hwblockdevice.h
+++ stack/framework/hal/inc/hwblockdevice.h
@@ -12,6 +12,8 @@ typedef struct {
   void (*init)(blockdevice_t* bd);
   error_t (*read)(blockdevice_t* bd, uint8_t* data, uint32_t addr, uint32_t size);
   error_t (*program)(blockdevice_t* bd, uint8_t* data, uint32_t addr, uint32_t size);
+  error_t (*erase_chip)(blockdevice_t* bd);
+  error_t (*erase_block32k)(blockdevice_t* bd, uint32_t addr);
 } blockdevice_driver_t;
 
 struct blockdevice {
@@ -21,6 +23,8 @@ struct blockdevice {
 void blockdevice_init(blockdevice_t* bd);
 error_t blockdevice_read(blockdevice_t* bd, uint8_t* data, uint32_t addr, uint32_t size);
 error_t blockdevice_program(blockdevice_t* bd, uint8_t* data, uint32_t addr, uint32_t size);
+error_t blockdevice_erase_chip(blockdevice_t* bd);
+error_t blockdevice_erase_block32k(blockdevice_t* bd, uint32_t addr);
 
 #endif
 
