d80a77fb10fe9b0480a343e73cede08695ae95a8
Philippe Nunes <philippe.nunes@cortus.com>
2019-04-19 15:16:36 +0200

    Revert: header for system files are correctly BE provisioned

--- stack/modules/d7ap/d7ap_fs.c
+++ stack/modules/d7ap/d7ap_fs.c
@@ -270,10 +270,11 @@ alp_status_codes_t d7ap_fs_read_file_header(uint8_t file_id, fs_file_header_t* f
     blockdevice_read(bd_systemfiles, (uint8_t*)file_header, systemfiles_header_offset + (file_id * sizeof(fs_file_header_t)), sizeof(fs_file_header_t));
   else {
     memcpy(file_header, (const void*)&fs_userfiles_header_data[get_user_file_header_index(file_id)], sizeof(fs_file_header_t));
-    // convert to little endian (native)
-    file_header->length = __builtin_bswap32(file_header->length);
-    file_header->allocated_length = __builtin_bswap32(file_header->allocated_length);
   }
+
+  // convert to little endian (native)
+  file_header->length = __builtin_bswap32(file_header->length);
+  file_header->allocated_length = __builtin_bswap32(file_header->allocated_length);
 
   return ALP_STATUS_OK;
 }
