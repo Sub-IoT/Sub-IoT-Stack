fiifth/dash7-ap-open-source-stack

Configuration settings caching
To reduce the latency due to repetitive persistent file access,
we do some caching of configuration parameters.
In case of file modification, the caching is updated accordingly.

stack/modules/d7ap/d7anp.c
@@ -71,6 +71,9 @@ static dae_nwl_trusted_node_t* NGDEF(_latest_node);
 static timer_event d7anp_fg_scan_expired_timer;
 static timer_event d7anp_start_fg_scan_after_d7aadvp_timer;
 
+d7ap_addressee_id_type_t address_id_type;
+uint8_t address_id[8];
+
 #if defined(MODULE_D7AP_NLS_ENABLED)
 static inline uint8_t get_auth_len(uint8_t nls_method)
 {
@@ -207,6 +210,19 @@ void d7anp_init()
     timer_init_event(&d7anp_fg_scan_expired_timer, &foreground_scan_expired);
     timer_init_event(&d7anp_start_fg_scan_after_d7aadvp_timer, &start_foreground_scan_after_D7AAdvP);
 
+    /*
+     * vid or uid caching to prevent latency due to file access
+     */
+    fs_read_vid(address_id);
+
+    // vid is not valid when set to FF
+    if (memcmp(address_id, (uint8_t[2]){ 0xFF, 0xFF }, 2) == 0)
+    {
+        fs_read_uid(address_id);
+        address_id_type = ID_TYPE_UID;
+    } else
+        address_id_type = ID_TYPE_VID;
+
 #if defined(MODULE_D7AP_NLS_ENABLED)
     /*
      * Init Security
@@ -256,13 +272,7 @@ error_t d7anp_tx_foreground_frame(packet_t* packet, bool should_include_origin_t
     }
     else
     {
-        uint8_t vid[2];
-        fs_read_vid(vid);
-        if (memcmp(vid, (uint8_t[2]){ 0xFF, 0xFF }, 2) == 0)
-            packet->d7anp_ctrl.origin_id_type = ID_TYPE_UID;
-        else
-            packet->d7anp_ctrl.origin_id_type = ID_TYPE_VID;
-
+        packet->d7anp_ctrl.origin_id_type = address_id_type;
         packet->d7anp_ctrl.origin_void = false;
         // note we set packet->origin_access_class in DLL, since we cache the active access class there already
     }
@@ -539,14 +549,12 @@ uint8_t d7anp_assemble_packet_header(packet_t *packet, uint8_t *data_ptr)
 
         if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_UID)
         {
-            fs_read_uid(data_ptr);
-            memcpy(packet->origin_access_id, data_ptr, 8);
+            memcpy(packet->origin_access_id, address_id, 8);
             data_ptr += 8;
         }
         else if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_VID)
         {
-            fs_read_vid(data_ptr);
-            memcpy(packet->origin_access_id, data_ptr, 2);
+            memcpy(packet->origin_access_id, address_id, 2);
             data_ptr += 2;
         }
         else if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_NBID)

stack/modules/d7ap/dll.c
@@ -674,13 +674,6 @@ void dll_execute_scan_automation(void *arg)
     // since they might not be necessary for current active class anymore
     timer_cancel_event(&dll_background_scan_timer);
 
-    uint8_t scan_access_class = fs_read_dll_conf_active_access_class();
-    if (active_access_class != scan_access_class)
-    {
-        fs_read_access_class(ACCESS_SPECIFIER(scan_access_class), &current_access_profile);
-        active_access_class = scan_access_class;
-    }
-
     DPRINT("DLL execute scan autom AC=0x%02x", active_access_class);
 
     /*
@@ -759,24 +752,36 @@ void dll_execute_scan_automation(void *arg)
 static void conf_file_changed_callback(uint8_t file_id)
 {
     DPRINT("DLL config file changed");
-    // when doing scan automation restart this
-    if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+    uint8_t scan_access_class = fs_read_dll_conf_active_access_class();
+
+    // if the access class has been modified, update the current access profile
+    if (active_access_class != scan_access_class)
     {
-        dll_execute_scan_automation(NULL);
+        fs_read_access_class(ACCESS_SPECIFIER(scan_access_class), &current_access_profile);
+        active_access_class = scan_access_class;
+
+        // when doing scan automation restart this
+        if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+        {
+            dll_execute_scan_automation(NULL);
+        }
     }
 }
 
 static void access_profile_file_changed_callback(uint8_t file_id)
 {
-    DPRINT("AP file changed");
+    DPRINT("Access Profile changed");
 
-    // make sure access class is re-read when entering scan automation
-    active_access_class = NO_ACTIVE_ACCESS_CLASS;
-
-    // when we are idle switch to scan automation now as well, in case the new AP enables scanning
-    if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+    // update only the current access profile if this access profile has been changed
+    if (file_id == D7A_FILE_ACCESS_PROFILE_ID + ACCESS_SPECIFIER(active_access_class))
     {
-        dll_execute_scan_automation(NULL);
+        fs_read_access_class(ACCESS_SPECIFIER(active_access_class), &current_access_profile);
+
+        // when we are idle switch to scan automation now as well, in case the new AP enables scanning
+        if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+        {
+            dll_execute_scan_automation(NULL);
+        }
     }
 }
 
@@ -806,7 +811,11 @@ void dll_init()
     tx_nf_method = (nf_ctrl >> 4) & 0x0F;
 
     dll_state = DLL_STATE_IDLE;
-    active_access_class = NO_ACTIVE_ACCESS_CLASS;
+
+    // caching of the active class and the selected access profile
+    active_access_class = fs_read_dll_conf_active_access_class();
+    fs_read_access_class(ACCESS_SPECIFIER(active_access_class), &current_access_profile);
+
     process_received_packets_after_tx = false;
     resume_fg_scan = false;
 

Sub-IoT/Sub-IoT-Stack

stack/modules/d7ap/d7anp.c
@@ -71,6 +71,9 @@ static dae_nwl_trusted_node_t* NGDEF(_latest_node);
 static timer_event d7anp_fg_scan_expired_timer;
 static timer_event d7anp_start_fg_scan_after_d7aadvp_timer;
 
+d7ap_addressee_id_type_t address_id_type;
+uint8_t address_id[8];
+
 #if defined(MODULE_D7AP_NLS_ENABLED)
 static inline uint8_t get_auth_len(uint8_t nls_method)
 {
@@ -210,6 +213,19 @@ void d7anp_init()
     timer_init_event(&d7anp_fg_scan_expired_timer, &foreground_scan_expired);
     timer_init_event(&d7anp_start_fg_scan_after_d7aadvp_timer, &start_foreground_scan_after_D7AAdvP);
 
+    /*
+     * vid or uid caching to prevent latency due to file access
+     */
+    d7ap_fs_read_vid(address_id);
+
+    // vid is not valid when set to FF
+    if (memcmp(address_id, (uint8_t[2]){ 0xFF, 0xFF }, 2) == 0)
+    {
+        d7ap_fs_read_uid(address_id);
+        address_id_type = ID_TYPE_UID;
+    } else
+        address_id_type = ID_TYPE_VID;
+
 #if defined(MODULE_D7AP_NLS_ENABLED)
     /*
      * Init Security
@@ -259,13 +275,7 @@ error_t d7anp_tx_foreground_frame(packet_t* packet, bool should_include_origin_t
     }
     else
     {
-        uint8_t vid[2];
-        d7ap_fs_read_vid(vid);
-        if (memcmp(vid, (uint8_t[2]){ 0xFF, 0xFF }, 2) == 0)
-            packet->d7anp_ctrl.origin_id_type = ID_TYPE_UID;
-        else
-            packet->d7anp_ctrl.origin_id_type = ID_TYPE_VID;
-
+        packet->d7anp_ctrl.origin_id_type = address_id_type;
         packet->d7anp_ctrl.origin_void = false;
         // note we set packet->origin_access_class in DLL, since we cache the active access class there already
     }
@@ -543,14 +553,14 @@ uint8_t d7anp_assemble_packet_header(packet_t *packet, uint8_t *data_ptr)
 
         if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_UID)
         {
-            d7ap_fs_read_uid(data_ptr);
-            memcpy(packet->origin_access_id, data_ptr, 8);
+            memcpy(packet->origin_access_id, address_id, 8);
+            memcpy(data_ptr, address_id, 8);
             data_ptr += 8;
         }
         else if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_VID)
         {
-            d7ap_fs_read_vid(data_ptr);
-            memcpy(packet->origin_access_id, data_ptr, 2);
+            memcpy(packet->origin_access_id, address_id, 2);
+            memcpy(data_ptr, address_id, 2);
             data_ptr += 2;
         }
         else if (packet->d7anp_ctrl.origin_id_type == ID_TYPE_NBID)

stack/modules/d7ap/dll.c
@@ -681,13 +681,6 @@ void dll_execute_scan_automation(void *arg)
     // since they might not be necessary for current active class anymore
     timer_cancel_event(&dll_background_scan_timer);
 
-    uint8_t scan_access_class = d7ap_fs_read_dll_conf_active_access_class();
-    if (active_access_class != scan_access_class)
-    {
-        d7ap_fs_read_access_class(ACCESS_SPECIFIER(scan_access_class), &current_access_profile);
-        active_access_class = scan_access_class;
-    }
-
     DPRINT("DLL execute scan autom AC=0x%02x", active_access_class);
 
     /*
@@ -767,24 +760,36 @@ static void conf_file_changed_callback(uint8_t file_id)
 {
     (void)file_id;
     DPRINT("DLL config file changed");
-    // when doing scan automation restart this
-    if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+    uint8_t scan_access_class = d7ap_fs_read_dll_conf_active_access_class();
+
+    // if the access class has been modified, update the current access profile
+    if (active_access_class != scan_access_class)
     {
-        sched_post_task(&dll_execute_scan_automation);
+        d7ap_fs_read_access_class(ACCESS_SPECIFIER(scan_access_class), &current_access_profile);
+        active_access_class = scan_access_class;
+
+        // when doing scan automation restart this
+        if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+        {
+            sched_post_task(&dll_execute_scan_automation);
+        }
     }
 }
 
 void dll_notify_access_profile_file_changed(uint8_t file_id)
 {
-    DPRINT("AP file changed");
+    DPRINT("Access Profile changed");
 
-    // make sure access class is re-read when entering scan automation
-    active_access_class = NO_ACTIVE_ACCESS_CLASS;
-
-    // when we are idle switch to scan automation now as well, in case the new AP enables scanning
-    if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+    // update only the current access profile if this access profile has been changed
+    if (file_id == D7A_FILE_ACCESS_PROFILE_ID + ACCESS_SPECIFIER(active_access_class))
     {
-        sched_post_task(&dll_execute_scan_automation);
+        d7ap_fs_read_access_class(ACCESS_SPECIFIER(active_access_class), &current_access_profile);
+
+        // when we are idle switch to scan automation now as well, in case the new AP enables scanning
+        if (dll_state == DLL_STATE_IDLE || dll_state == DLL_STATE_SCAN_AUTOMATION)
+        {
+            sched_post_task(&dll_execute_scan_automation);
+        }
     }
 }
 
@@ -816,7 +821,11 @@ void dll_init()
     tx_nf_method = (nf_ctrl >> 4) & 0x0F;
 
     dll_state = DLL_STATE_IDLE;
-    active_access_class = NO_ACTIVE_ACCESS_CLASS;
+
+    // caching of the active class and the selected access profile
+    active_access_class = d7ap_fs_read_dll_conf_active_access_class();
+    d7ap_fs_read_access_class(ACCESS_SPECIFIER(active_access_class), &current_access_profile);
+
     process_received_packets_after_tx = false;
     resume_fg_scan = false;
 