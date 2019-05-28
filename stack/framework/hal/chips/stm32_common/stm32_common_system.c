09e5cbdc7980d6527686f7d7e76ff558d97227e1
Liam Oorts <liam.oorts@aloxy.io>
2019-05-28 14:22:45 +0200

    now not powering down flash anymore in sleep mode (framework_scheduler_lp_mode 0), caused hardfaults. Also added new hardfault handling in comments for debugging purposes.

--- stack/framework/hal/chips/stm32_common/stm32_common_system.c
+++ stack/framework/hal/chips/stm32_common/stm32_common_system.c
@@ -130,6 +130,7 @@ void hw_enter_lowpower_mode(uint8_t mode)
   switch (mode)
   {
     case 0: // sleep mode
+      __HAL_FLASH_SLEEP_POWERDOWN_DISABLE(); //TODO Not optimised, without this Sleepmode 
       HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
       break;
     case 1: // STOP mode
@@ -223,4 +224,72 @@ void HardFault_Handler()
       "str_hardfault: .asciz \"HardFault\"\n\t"
   );
 }
+
+//To debug hardfault
+// void HardFault_HandlerC(unsigned long *hardfault_args){
+//   volatile unsigned long stacked_r0 ;
+//   volatile unsigned long stacked_r1 ;
+//   volatile unsigned long stacked_r2 ;
+//   volatile unsigned long stacked_r3 ;
+//   volatile unsigned long stacked_r12 ;
+//   volatile unsigned long stacked_lr ;
+//   volatile unsigned long stacked_pc ;
+//   volatile unsigned long stacked_psr ;
+//   volatile unsigned long _CFSR ;
+//   volatile unsigned long _HFSR ;
+//   volatile unsigned long _DFSR ;
+//   volatile unsigned long _AFSR ;
+//   volatile unsigned long _BFAR ;
+//   volatile unsigned long _MMAR ;
+ 
+//   stacked_r0 = ((unsigned long)hardfault_args[0]) ;
+//   stacked_r1 = ((unsigned long)hardfault_args[1]) ;
+//   stacked_r2 = ((unsigned long)hardfault_args[2]) ;
+//   stacked_r3 = ((unsigned long)hardfault_args[3]) ;
+//   stacked_r12 = ((unsigned long)hardfault_args[4]) ;
+//   stacked_lr = ((unsigned long)hardfault_args[5]) ;
+//   stacked_pc = ((unsigned long)hardfault_args[6]) ;
+//   stacked_psr = ((unsigned long)hardfault_args[7]) ;
+ 
+//   // Configurable Fault Status Register
+//   // Consists of MMSR, BFSR and UFSR
+//   _CFSR = (*((volatile unsigned long *)(0xE000ED28))) ;
+ 
+//   // Hard Fault Status Register
+//   _HFSR = (*((volatile unsigned long *)(0xE000ED2C))) ;
+ 
+//   // Debug Fault Status Register
+//   _DFSR = (*((volatile unsigned long *)(0xE000ED30))) ;
+ 
+//   // Auxiliary Fault Status Register
+//   _AFSR = (*((volatile unsigned long *)(0xE000ED3C))) ;
+ 
+//   // Read the Fault Address Registers. These may not contain valid values.
+//   // Check BFARVALID/MMARVALID to see if they are valid values
+//   // MemManage Fault Address Register
+//   _MMAR = (*((volatile unsigned long *)(0xE000ED34))) ;
+//   // Bus Fault Address Register
+//   _BFAR = (*((volatile unsigned long *)(0xE000ED38))) ;
+ 
+//   __asm("BKPT #0\n") ; // Break into the debugger
+// }
+ 
+// __attribute__((naked)) void HardFault_Handler();
+// void HardFault_Handler()
+// {
+//   __asm volatile (
+//     " movs r0,#4       \n"
+//     " movs r1, lr      \n"
+//     " tst r0, r1       \n"
+//     " beq _MSP         \n"
+//     " mrs r0, psp      \n"
+//     " b _HALT          \n"
+//   "_MSP:               \n"
+//     " mrs r0, msp      \n"
+//   "_HALT:              \n"
+//     " ldr r1,[r0,#20]  \n"
+//     " b HardFault_HandlerC \n"
+//     " bkpt #0          \n"
+//   );
+// }
 
