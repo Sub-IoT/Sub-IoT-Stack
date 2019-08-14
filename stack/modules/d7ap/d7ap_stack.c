Sub-IoT/Sub-IoT-Stack

commit c202809fd24554391bccf7870d942394329d3b76

Fixed a bug where in case a two successive sessions are created with the same token,  the old (now inactive) session is assigned. This leads to a session with an invalid client ID thus discarding any received responses

stack/modules/d7ap/d7ap_stack.c
@@ -234,7 +234,7 @@ error_t d7ap_stack_send(uint8_t client_id, d7ap_session_config_t* config, uint8_
     // Check if a session already exists
     session_t* session = get_session_by_session_token(session_token);
 
-    if (session == NULL) {
+    if ((session == NULL) || (session->active == false)) {
         session = alloc_session(client_id);
 
         if (session == NULL)

commit bc3f5f8b3f7c13c2d93999754b88c9cbe8279d90

d7ap_stack: fix regression in the session allocation
This regression introduced by the patch c202809 is found when
a created session remains pending (id not active).

stack/modules/d7ap/d7ap_stack.c
@@ -248,7 +248,7 @@ error_t d7ap_stack_send(uint8_t client_id, d7ap_session_config_t* config, uint8_
     // Check if a session already exists
     session_t* session = get_session_by_session_token(session_token);
 
-    if ((session == NULL) || (session->active == false)) {
+    if (session == NULL) {
         session = alloc_session(client_id);
 
         if (session == NULL)
@@ -270,6 +270,8 @@ error_t d7ap_stack_send(uint8_t client_id, d7ap_session_config_t* config, uint8_
 
     if (trans_id != NULL)
         *trans_id = session->trans_id[session->request_nb];
+
+    DPRINT("[D7AP] request posted with trans_id %02X and request_nb %d", *trans_id, session->request_nb);
 
     session->request_nb++;
     return SUCCESS;
@@ -311,9 +313,13 @@ void d7ap_stack_process_received_response(uint8_t *payload, uint8_t length, d7ap
     uint16_t trans_id = ((uint16_t)result.fifo_token << 8) | (result.seqnr & 0x00FF);
     uint8_t i = 0;
 
+    DPRINT("[D7AP] received trans_id %02X", trans_id);
+    DPRINT("[D7AP] session->request_nb %d", session->request_nb);
+
     for(i = 0; i < session->request_nb; i++)
     {
-        if (session->trans_id[i] == trans_id)
+    	DPRINT("[D7AP] session->trans_id[i] = %02X", session->trans_id[i]);
+    	if (session->trans_id[i] == trans_id)
             break;
     }
 

stack/modules/d7ap/d7asp.c
@@ -215,6 +215,7 @@ static void flush_fifos()
         }
 
         current_request_id = found_next_req_index;
+        DPRINT("Found request Id %x", current_request_id);
         current_request_retry_count = 0;
 
         current_request_packet = packet_queue_alloc_packet();
@@ -410,6 +411,9 @@ uint8_t d7asp_master_session_create(d7ap_session_config_t* d7asp_master_session_
         // TODO create a pending session or a dormant session if TO (DORM_TIMER) !=0
     }
 
+    DPRINT("current master session state %d", current_master_session.state);
+
+
     init_master_session(&current_master_session);
 
     DPRINT("Create master session %d", current_master_session.token);
@@ -555,7 +559,7 @@ void d7asp_process_received_response(packet_t* packet, bool extension)
     assert(packet->d7atp_transaction_id == current_request_id);
 
     // received ack
-    DPRINT("Received ACK");
+    DPRINT("Received ACK for request ID %d", current_request_id);
     if (current_master_session.config.qos.qos_resp_mode != SESSION_RESP_MODE_NO
        && current_master_session.config.qos.qos_resp_mode != SESSION_RESP_MODE_NO_RPT)
     {