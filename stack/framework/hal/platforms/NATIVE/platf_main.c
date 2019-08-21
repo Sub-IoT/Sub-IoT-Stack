6d027cd58870e00e5747ab99f4501ad771cdd687
Glenn Ergeerts <glenn.ergeerts@aloxy.io>
2019-08-21 10:08:41 +0200

    re-enabled hw_get_unique_id(), native platform now returns harcoded value for this

--- stack/framework/hal/platforms/NATIVE/platf_main.c
+++ stack/framework/hal/platforms/NATIVE/platf_main.c
@@ -65,5 +65,5 @@ __LINK_C error_t hw_timer_schedule(hwtimer_id_t timer_id, hwtimer_tick_t tick ) 
 __LINK_C error_t hw_timer_init(hwtimer_id_t timer_id, uint8_t frequency, timer_callback_t compare_callback, timer_callback_t overflow_callback) {}
 __LINK_C bool hw_timer_is_overflow_pending(hwtimer_id_t id) {}
 __LINK_C error_t hw_timer_cancel(hwtimer_id_t timer_id) {}
-__LINK_C uint64_t hw_get_unique_id(void) {}
+__LINK_C uint64_t hw_get_unique_id(void) { return 0xFFFFFFFFFFFFFF;}
 
