Sub-IoT/Sub-IoT-Stack

commit f95d8943f29af41952fc3d061005ca0f79e89b2f

commented out stop_transaction

stack/modules/d7ap/d7asp.c
@@ -600,7 +600,7 @@ void d7asp_process_received_response(packet_t* packet, bool extension)
         current_request_id = NO_ACTIVE_REQUEST_ID;
         schedule_current_session(); // continue flushing until all request handled ...
         // stop the current transaction
-        d7atp_stop_transaction();
+        // d7atp_stop_transaction(); //TO BE CHECKED THAT COMMENTING THIS OUT HAS NO NEGATIVE EFFECT
     }
     // switch to the state slave when the D7ATP Dialog Extension Procedure is initiated and all request are handled
     else if ((extension) && (current_request_id == current_master_session.next_request_id - 1))

commit d7f5a180ebd3997ea473b548661384d5419a52b0

[To be confirmed] d7asp: fix regression introduced by the commit f95d894
Transport layer needs to be informed that the transaction is terminated.

stack/modules/d7ap/d7asp.c
@@ -710,7 +710,7 @@ void d7asp_process_received_response(packet_t* packet, bool extension)
         current_request_id = NO_ACTIVE_REQUEST_ID;
         schedule_current_session(); // continue flushing until all request handled ...
         // stop the current transaction
-        // d7atp_stop_transaction(); //TO BE CHECKED THAT COMMENTING THIS OUT HAS NO NEGATIVE EFFECT
+        d7atp_stop_transaction(); //TO BE CHECKED THAT COMMENTING THIS OUT HAS NO NEGATIVE EFFECT
     }
     // switch to the state slave when the D7ATP Dialog Extension Procedure is initiated and all request are handled
     else if ((extension) && (current_request_id == current_master_session->next_request_id - 1))