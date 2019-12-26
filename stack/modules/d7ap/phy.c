017feda68a46c7d6af5f1d04319ceb0ade6d5dbd
Liam Oorts <liam.oorts@aloxy.io>
2019-12-26 14:23:55 +0100

    implement dll status file as volatile

--- stack/modules/d7ap/phy.c
+++ stack/modules/d7ap/phy.c
@@ -181,9 +181,8 @@ static uint8_t rssi_offset;
 
 static uint16_t total_bg = 0;
 static uint16_t total_rssi_triggers = 0;
-static uint16_t total_fg = 0;
+static uint16_t total_fg = 1;
 static uint16_t total_succeeded_fg = 0;
-static uint8_t write_file_counter = 0;
 
 static uint8_t gain_offset = 0;
 
@@ -627,15 +626,10 @@ error_t phy_init(void) {
 }
 
 void status_write() {
-    write_file_counter++;
-    if(write_file_counter == 100) {
-        write_file_counter = 0;
-        uint16_t bg_trigger_ratio = 1024 * total_rssi_triggers / total_bg;
-        uint16_t scan_timeout_ratio = 1024 * (total_fg - total_succeeded_fg) / total_fg;
-        uint8_t buffer[4] = {(uint8_t)(bg_trigger_ratio >> 8), (uint8_t)(bg_trigger_ratio & 0xFF), (uint8_t)(scan_timeout_ratio >> 8), (uint8_t)(scan_timeout_ratio & 0xFF)};
-        d7ap_fs_write_file(D7A_FILE_DLL_STATUS_FILE_ID, 8, buffer, 4);
-        DPRINT("wrote to file 0x%02X the bg trigger ratio %d and scan timeout ratio %d", D7A_FILE_DLL_STATUS_FILE_ID, bg_trigger_ratio, scan_timeout_ratio);
-    }
+    uint16_t bg_trigger_ratio = 1024 * total_rssi_triggers / total_bg;
+    uint16_t scan_timeout_ratio = 1024 * (total_fg - total_succeeded_fg) / total_fg;
+    uint8_t buffer[4] = {(uint8_t)(bg_trigger_ratio >> 8), (uint8_t)(bg_trigger_ratio & 0xFF), (uint8_t)(scan_timeout_ratio >> 8), (uint8_t)(scan_timeout_ratio & 0xFF)};
+    d7ap_fs_write_file(D7A_FILE_DLL_STATUS_FILE_ID, 8, buffer, 4);
 }
 
 error_t phy_start_rx(channel_id_t* channel, syncword_class_t syncword_class, phy_rx_packet_callback_t rx_cb) {
