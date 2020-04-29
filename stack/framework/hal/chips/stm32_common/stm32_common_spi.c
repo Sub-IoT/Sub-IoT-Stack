d978008b11c02ac0329599de8583fc8574577e99
Liam Oorts <liam.oorts@aloxy.io>
2020-04-29 10:27:03 +0200

    assert did not rise anymore

--- stack/framework/hal/chips/stm32_common/stm32_common_spi.c
+++ stack/framework/hal/chips/stm32_common/stm32_common_spi.c
@@ -31,6 +31,7 @@
 #include "ports.h"
 #include "hwgpio.h"
 #include "errors.h"
+#include "hwatomic.h"
 
 
 #define MAX_SPI_SLAVE_HANDLES 5        // TODO expose this in chip configuration
@@ -108,6 +109,7 @@ void spi_enable(spi_handle_t* spi) {
       __HAL_RCC_SPI2_CLK_ENABLE();
       break;
     default:
+      log_print_string("instance is 0x%08X with portnumber %i compared to bases %08X and %08X\n", (uint32_t)spi->hspi.Instance, spi->spi_port_number,(uint32_t) SPI1_BASE, (uint32_t) SPI2_BASE);
       assert(false);
   }
 
@@ -282,6 +284,7 @@ void spi_send_byte_with_control(spi_slave_handle_t* slave, uint16_t data) {
 }
 
 void spi_exchange_bytes(spi_slave_handle_t* slave, uint8_t* TxData, uint8_t* RxData, size_t length) {
+  start_atomic();
   // TODO replace HAL calls with direct registry access for performance / code size ?
   if( RxData != NULL && TxData != NULL ) {           // two way transmission
     HAL_SPI_TransmitReceive(&slave->spi->hspi, TxData, RxData, length, HAL_MAX_DELAY);
@@ -291,13 +294,14 @@ void spi_exchange_bytes(spi_slave_handle_t* slave, uint8_t* TxData, uint8_t* RxD
     if(slave->spi->hspi.Init.Direction == SPI_DIRECTION_2LINES) {
       HAL_SPI_Receive(&slave->spi->hspi, RxData, length, HAL_MAX_DELAY);
     } else {
+      assert(length == 1); //rest is not supported as for now
       // in 3 wire mode RX the SPI pheriperal seems unable to stock clocking after receiving the expected number of bytes
       // disabling the SPI by polling until RXNE is set will result in too many clocks being generated, causing the slave to clokc out more bytes then needed.
       // This can be problematic for some SPI slaves for example when reading a FIFO, where the slave will return bytes from the FIFO but these will be missed by the master.
       // The workaround used by STM in the LMS303 sample of the STM32L4 discovery board is to disable the clock manually after a few cycles, instead of polling RXNE.
       // We use a similar implementation for now
       __SPI_DIRECTION_1LINE_RX(&slave->spi->hspi);
-      for(size_t i = 0; i < length; i++) {
+      // for(size_t i = 0; i < length; i++) {
         __HAL_SPI_ENABLE(&slave->spi->hspi);
         __DSB();
         __DSB();
@@ -309,9 +313,10 @@ void spi_exchange_bytes(spi_slave_handle_t* slave, uint8_t* TxData, uint8_t* RxD
         __DSB();
         __HAL_SPI_DISABLE(&slave->spi->hspi);
         while(!(slave->spi->hspi.Instance->SR & SPI_FLAG_RXNE));
-        RxData[i] = *((__IO uint8_t *)&slave->spi->hspi.Instance->DR);
+        RxData[0] = *((__IO uint8_t *)&slave->spi->hspi.Instance->DR);
         while((slave->spi->hspi.Instance->SR & SPI_FLAG_BSY) == SPI_FLAG_BSY);
-      }
+      // }
     }
   }
+  end_atomic();
 }
